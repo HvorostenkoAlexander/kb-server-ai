d714fb482f3d668141d6fb5297a8e4895ab9040a
Носов Сергей Владимирович (nosov_sv) <nosov_sv@nlmk.com>
2021-04-13 11:08:32 +0900

    настройка kafkaSsl directory

--- src/main/java/com/nlmk/kb/server/KbServerApplication.java
+++ src/main/java/com/nlmk/kb/server/KbServerApplication.java
@@ -2,11 +2,17 @@ package com.nlmk.kb.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.util.FileCopyUtils;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 @SpringBootApplication
 public class KbServerApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         SpringApplication.run(KbServerApplication.class, args);
     }
 
