5dc07486ad0a3f6d81320a4b49a84a4944c38d41
Носов Сергей Владимирович (nosov_sv) <nosov_sv@nlmk.com>
2021-04-16 20:49:52 +0900

    установка initialOffset

--- src/main/java/com/nlmk/kb/server/service/SupService.java
+++ src/main/java/com/nlmk/kb/server/service/SupService.java
@@ -3,6 +3,8 @@ package com.nlmk.kb.server.service;
 import lombok.extern.slf4j.Slf4j;
 import nlmk.l3.sup.IntegralParameters;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.PartitionOffset;
+import org.springframework.kafka.annotation.TopicPartition;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
 
@@ -10,8 +12,16 @@ import org.springframework.stereotype.Service;
 @Service
 public class SupService {
 
-    @KafkaListener(topics = {"${kafka.sup.topicIp}"},
-            containerFactory = "kafkaListenerContainerFactoryIp")
+//    @TopicPartition(topic = "topic2", partitions = "0",
+//            partitionOffsets = @PartitionOffset(partition = "1", initialOffset = "100")
+//@KafkaListener(topics = {"${kafka.sup.topicIp}"},
+//        containerFactory = "kafkaListenerContainerFactoryIp")
+
+
+    @KafkaListener(containerFactory = "kafkaListenerContainerFactoryIp",
+                    topicPartitions = {@TopicPartition(topic = "topic2", partitions = "0",
+                                    partitionOffsets =
+                                    @PartitionOffset(partition = "0", initialOffset = "24")),})
     public void receiveMessage(@Payload String supIntegralParameters) {
 
         log.info("--- received integralParameters: {}", supIntegralParameters);
