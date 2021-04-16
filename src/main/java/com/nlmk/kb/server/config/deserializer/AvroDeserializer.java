c4308bc1f3b7793b519de8b2c568e7047196a0b8
Носов Сергей Владимирович (nosov_sv) <nosov_sv@nlmk.com>
2021-04-16 19:22:45 +0900

    логирование в  AvroDeserializer

--- src/main/java/com/nlmk/kb/server/config/deserializer/AvroDeserializer.java
+++ src/main/java/com/nlmk/kb/server/config/deserializer/AvroDeserializer.java
@@ -41,7 +41,11 @@ public class AvroDeserializer<T extends SpecificRecordBase> implements Deseriali
 
                 DatumReader<GenericRecord> datumReader =
                         new SpecificDatumReader<>(targetType.getDeclaredConstructor().newInstance().getSchema());
+                log.info("--- datumReader: "+datumReader.toString());
+
                 Decoder decoder = DecoderFactory.get().binaryDecoder(data, null);
+                log.info("--- decoder: "+decoder.toString());
+                log.info("--- decoder.readString"+decoder.readString());
 
                 result = (T) datumReader.read(null, decoder);
                 log.info("deserialized data='{}'", result);
