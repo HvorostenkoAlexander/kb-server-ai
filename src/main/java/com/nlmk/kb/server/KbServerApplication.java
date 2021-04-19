6422a95e42c6edffec6c8bd81e0c2dc4ea727322
Носов Сергей Владимирович (nosov_sv) <nosov_sv@nlmk.com>
2021-04-19 15:58:37 +0900

    Merge branch 'profile' into development

--- src/main/java/com/nlmk/kb/server/KbServerApplication.java
+++ src/main/java/com/nlmk/kb/server/KbServerApplication.java
@@ -4,6 +4,8 @@ import com.nlmk.kb.server.config.SupConsumerProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.StandardEnvironment;
 import org.springframework.kafka.annotation.EnableKafka;
 
 @EnableKafka
@@ -12,8 +14,14 @@ import org.springframework.kafka.annotation.EnableKafka;
 public class KbServerApplication {
 
     public static void main(String[] args) {
+        //SpringApplication.run(KbServerApplication.class, args);
 
-        SpringApplication.run(KbServerApplication.class, args);
+        SpringApplication application = new SpringApplication(KbServerApplication.class);
+        ConfigurableEnvironment environment = new StandardEnvironment();
+        environment.setActiveProfiles("prod");
+        application.setEnvironment(environment);
+
+        application.run(args);
     }
 
 }
