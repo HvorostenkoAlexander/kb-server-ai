8dee947ba6267b47f202957e6fd77c5c39e3f05c
Носов Сергей Владимирович (nosov_sv) <nosov_sv@nlmk.com>
2021-04-20 15:13:14 +0900

    посмотреть все заголовки сообщения

--- src/main/java/com/nlmk/kb/server/service/SupService.java
+++ src/main/java/com/nlmk/kb/server/service/SupService.java
@@ -9,7 +9,9 @@ import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.annotation.PartitionOffset;
 import org.springframework.kafka.annotation.TopicPartition;
 import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.MessageHeaders;
 import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.messaging.handler.annotation.Headers;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
 
@@ -26,13 +28,18 @@ public class SupService {
             topicPartitions = {@TopicPartition(topic = "${kafka.sup.topicIp}",
                     partitionOffsets =
                     @PartitionOffset(partition = "0", initialOffset = "0")),})
-    public void receiveMessage(@Header(KafkaHeaders.RECEIVED_MESSAGE_KEY) String key,
-                               @Header(KafkaHeaders.TIMESTAMP) String timestamp,
-                               @Header(KafkaHeaders.OFFSET) String offset,
+    public void receiveMessage(@Headers MessageHeaders headers,
                                @Payload IntegralParameters supIntegralParameters) {
-        log.info("--- received message: Key: {} ; Timestamp: {}; offset: {}; value:{}",
-                key, timestamp, offset,
-                supIntegralParameters);
+//        log.info("--- received message: Key: {} ; Timestamp: {}; offset: {}; value:{}",
+//                key, timestamp, offset,
+//                supIntegralParameters);
+//        @Header(KafkaHeaders.RECEIVED_MESSAGE_KEY) String key,
+//        @Header(KafkaHeaders.TIMESTAMP) String timestamp,
+//        @Header(KafkaHeaders.OFFSET) String offset,
+
+        headers.keySet().forEach(key -> {
+            log.info("--- {}: {}", key, headers.get(key));
+        });
 
         IntegralParam ip = ParamConverter.toIntegralParam(supIntegralParameters);
 
