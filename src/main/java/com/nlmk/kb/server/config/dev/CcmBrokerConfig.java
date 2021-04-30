053fc36eb3731e5f4d53be849b381a2454e7c552
Носов Сергей Владимирович (nosov_sv) <nosov_sv@nlmk.com>
2021-04-30 13:52:34 +0900

    рефакторинг CcmBrokerConfig (dev)

--- src/main/java/com/nlmk/kb/server/config/dev/CcmBrokerConfig.java
+++ src/main/java/com/nlmk/kb/server/config/dev/CcmBrokerConfig.java
@@ -1,8 +1,10 @@
 package com.nlmk.kb.server.config.dev;
 
 import com.nlmk.kb.server.config.CcmConsumerProperties;
+import com.nlmk.kb.server.config.dev.deserializer.AvroDeserializer;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import nlmk.l3.ccm.pgp.AttestationRequest;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.context.annotation.Bean;
@@ -27,56 +29,32 @@ public class CcmBrokerConfig {
     public Map<String, Object> ccmConsumerConfigs() {
         Map<String, Object> props = new HashMap<>();
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, consumerProperties.getKafkaServer());
-//        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, KafkaAvroDeserializer.class);
-//        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, KafkaAvroDeserializer.class);
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, AvroDeserializer.class);
         props.put(ConsumerConfig.GROUP_ID_CONFIG, consumerProperties.getKafkaGroupId());
         props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
         props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false);
-//        props.put("schema.registry.url", supConsumerProperties.getSchemaRegistryUrl());
-//        props.put("specific.avro.reader", "true");
 
         return props;
     }
 
     @Bean
-    public ConsumerFactory<String, String> ccmConsumerFactory() {
+    public ConsumerFactory<String, AttestationRequest> ccmConsumerFactory() {
 
-//        KafkaAvroDeserializer keyDeserializer = new KafkaAvroDeserializer();
-//        keyDeserializer.configure(consumerConfigs(), true);
-//
-//        KafkaAvroDeserializer valueDeserializer = new KafkaAvroDeserializer();
-//        valueDeserializer.configure(consumerConfigs(), false);
-
-//        ErrorHandlingDeserializer<Object> errorHandlingValueDeserializer
-//                = new ErrorHandlingDeserializer<>(valueDeserializer);
-//
-//        return new DefaultKafkaConsumerFactory<>(
-//                consumerConfigs(),
-//                keyDeserializer,
-//                errorHandlingValueDeserializer
-//        );
         return new DefaultKafkaConsumerFactory<>(
                 ccmConsumerConfigs(),
                 new StringDeserializer(),
-                new StringDeserializer()
+                new AvroDeserializer<>(AttestationRequest.class)
         );
     }
 
     @Bean
-    public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactoryReq() {
+    public ConcurrentKafkaListenerContainerFactory<String, AttestationRequest> kafkaListenerContainerFactoryReq() {
 
-        ConcurrentKafkaListenerContainerFactory<String, String> factory =
+        ConcurrentKafkaListenerContainerFactory<String, AttestationRequest> factory =
                 new ConcurrentKafkaListenerContainerFactory<>();
 
         factory.setConsumerFactory(ccmConsumerFactory());
-//        factory.setErrorHandler(new SeekToCurrentErrorHandler(
-//                (record, error) -> {
-//                    log.error("--- ERROR: "+error.getMessage());
-//                    log.error("--- ERROR RECORD: "+record.toString());
-//                }, new FixedBackOff(5000L, 1))
-//        );
         factory.setConcurrency(1);
 
         return factory;
