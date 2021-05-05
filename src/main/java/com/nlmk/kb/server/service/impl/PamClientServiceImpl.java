33e87e2149ebc35db5b54437be572cafff78960c
Носов Сергей Владимирович (nosov_sv) <nosov_sv@nlmk.com>
2021-05-05 15:23:15 +0900

    enable PamClientServiceImpl

--- src/main/java/com/nlmk/kb/server/service/impl/PamClientServiceImpl.java
+++ src/main/java/com/nlmk/kb/server/service/impl/PamClientServiceImpl.java
@@ -13,32 +13,32 @@ import org.springframework.web.client.RestTemplate;
 @Service
 public class PamClientServiceImpl implements PamClientService {
 
-//    private final String pamUrl;
-//    private final RestTemplate restTemplate;
-//
-//    public PamClientServiceImpl( @Value("${pam.url}") String pamUrl,
-//                                 RestTemplate restTemplate) {
-//        this.pamUrl = pamUrl;
-//        this.restTemplate = restTemplate;
-//    }
+    private final String pamUrl;
+    private final RestTemplate restTemplate;
+
+    public PamClientServiceImpl( @Value("${pam.url}") String pamUrl,
+                                 RestTemplate restTemplate) {
+        this.pamUrl = pamUrl;
+        this.restTemplate = restTemplate;
+    }
 
     @Override
     public void postAttestationRequest(com.nlmk.kb.server.entity.pam.AttestationRequest pamAttestetionRequest) {
 //       // с целью проверки работы exception handler
 //       // this.generateException(pamAttestetionRequest.getValue().getOp());
-//
-//        log.info("--- request: " + pamAttestetionRequest.getValue().getPk());
-//
-//        String authHeaderValue = "Authorization: Bearer XYZ";//todo правильно получить authHeaderValue
-//
-//        HttpHeaders header = new HttpHeaders();
-//        if (authHeaderValue != null) {
-//            header.add(HttpHeaders.AUTHORIZATION, authHeaderValue);
-//        }
-//
-//        restTemplate.postForEntity(pamUrl,
-//                new HttpEntity<Object>(pamAttestetionRequest, header),
-//                com.nlmk.kb.server.entity.pam.AttestationRequest.class);
+
+        log.info("--- request: " + pamAttestetionRequest.getValue().getPk());
+
+        String authHeaderValue = "Authorization: Bearer XYZ";//todo правильно получить authHeaderValue
+
+        HttpHeaders header = new HttpHeaders();
+        if (authHeaderValue != null) {
+            header.add(HttpHeaders.AUTHORIZATION, authHeaderValue);
+        }
+
+        restTemplate.postForEntity(pamUrl,
+                new HttpEntity<Object>(pamAttestetionRequest, header),
+                com.nlmk.kb.server.entity.pam.AttestationRequest.class);
     }
 
     private void generateException(String operation){
