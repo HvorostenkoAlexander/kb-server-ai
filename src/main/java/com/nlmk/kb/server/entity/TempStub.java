43434a5aa0eef4c521cebdb633b2db1209e603f1
Носов Сергей Владимирович (nosov_sv) <nosov_sv@nlmk.com>
2021-05-05 10:28:24 +0900

    создание TempStub

---
+++ src/main/java/com/nlmk/kb/server/entity/TempStub.java
@@ -0,0 +1,47 @@
+package com.nlmk.kb.server.entity;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class TempStub {
+    @JsonProperty("primeID")
+    private String primeID; //"идентификатор"
+
+    @JsonProperty("t12_min")
+    private Long t12Min;// "Температура конца прокатки (мin)"
+
+    @JsonProperty("t12_max")
+    private Long t12Max;// "Температура конца прокатки (мах)"
+
+    @JsonProperty("tcm_min")
+    private Long tcmMin;// "Температура смотки (мin)"
+
+    @JsonProperty("tcm_max")
+    private Long tcmMax;// "Температура смотки (мах)"
+
+    @JsonProperty("PBI")
+    private Long pbi;// "Процент длины полосы, на которой ширина в допуске"
+
+    @JsonProperty("ProfFact")
+    private Long profFact;// "Профиль"
+
+    @JsonProperty("WedgeFact")
+    private Long wedgeFact;// "Клин"
+
+    @JsonProperty("SQC_CRIT_MAX")
+    private Long sqcCritMax;// "Наибольшая критичность дефекта на полосе"
+
+    @JsonProperty("PH_1SGP")
+    private Long ph1sgp;// "Процент длины полосы, на которой толщина входит в полный допуск"
+
+    @JsonProperty("PH_12SGP")
+    private Long ph12sgp;// "Процент длины полосы, на которой толщина входит в (1/2) допуска"
+
+    @JsonProperty("PH_23SGP")
+    private Long ph23sgp;// "Процент длины полосы, на которой толщина входит в (2/3) допуска"
+
+    @JsonProperty("lclThckng")
+    private Long lclThckng;// "Высота местных утолщений по ширине полосы"
+
+    @JsonProperty("estimate")
+    private Long estimate;// "Оценка годности полосы"
+}
