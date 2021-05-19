b800007e4509fe90937c67a98fe062d25e9cbaa0
Носов Сергей Владимирович (nosov_sv) <nosov_sv@nlmk.com>
2021-05-19 14:44:33 +0900

    настройка SadimBrokedConfig

--- src/main/java/com/nlmk/kb/server/config/prod/SadimBrokedConfig.java
+++ src/main/java/com/nlmk/kb/server/config/prod/SadimBrokedConfig.java
@@ -12,6 +12,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -27,7 +28,7 @@ public class SadimBrokedConfig {
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "nl-st-hkafka01.ao.nlmk:9092");
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
      //   props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, KafkaJsonSchemaDeserializer.class);
-        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
         props.put(ConsumerConfig.GROUP_ID_CONFIG, "apcs.kb.sadim");
         props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
        // props.put(KafkaJsonDeserializerConfig.JSON_VALUE_TYPE, JsonNode.class.getName());
@@ -35,7 +36,7 @@ public class SadimBrokedConfig {
         return new DefaultKafkaConsumerFactory<>(
                 props,
                 new StringDeserializer(),
-                new StringDeserializer()
+                new JsonDeserializer<>(String.class)
         );
     }
 
