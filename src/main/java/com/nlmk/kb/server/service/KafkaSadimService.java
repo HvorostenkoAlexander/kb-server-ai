ebad6ed3f942798bb3d7114085149ad97e8b4cb4
Носов Сергей Владимирович (nosov_sv) <nosov_sv@nlmk.com>
2021-05-20 15:58:27 +0900

    просмотр SADIM value

--- src/main/java/com/nlmk/kb/server/service/KafkaSadimService.java
+++ src/main/java/com/nlmk/kb/server/service/KafkaSadimService.java
@@ -12,11 +12,14 @@ import org.springframework.stereotype.Service;
 @Service
 public class KafkaSadimService {
 
+    //    @KafkaListener(containerFactory = "kafkaListenerSadim",
+//            topicPartitions = {@TopicPartition(topic = "PA-MU.NLMK.P3.HSM",
+//            partitionOffsets = @PartitionOffset(partition = "0", initialOffset = "0")
+//            ),
+//            })
     @KafkaListener(containerFactory = "kafkaListenerSadim",
-            topicPartitions = {@TopicPartition(topic = "PA-MU.NLMK.P3.HSM"
-        //    partitionOffsets = @PartitionOffset(partition = "0", initialOffset = "0")
-            ),
-            })
+            topics = {"PA-MU.NLMK.P3.HSM"}
+    )
     public void receiveMessageReq(@Payload ConsumerRecord consumerRecord) {
 
 //        log.info("SADIM data from topic: timestamp: {}, key:{}, offset:{}",
