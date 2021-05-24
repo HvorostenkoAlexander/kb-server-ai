a397bbd7e71a596845f2878c9988c103a8987d10
Носов Сергей Владимирович (nosov_sv) <nosov_sv@nlmk.com>
2021-05-24 16:15:27 +0900

    добавление errorHandler в kafkaListenerSadim

--- src/main/java/com/nlmk/kb/server/service/KafkaSadimService.java
+++ src/main/java/com/nlmk/kb/server/service/KafkaSadimService.java
@@ -1,14 +1,10 @@
 package com.nlmk.kb.server.service;
 
+import com.nlmk.kb.server.entity.PreAttestationParam;
 import lombok.extern.slf4j.Slf4j;
-import lombok.val;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.annotation.PartitionOffset;
-import org.springframework.kafka.annotation.TopicPartition;
-import org.springframework.kafka.support.KafkaHeaders;
-import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
 
@@ -18,25 +14,23 @@ import java.util.Optional;
 @Service
 public class KafkaSadimService {
     private final SadimJsonParser sadimJsonParser;
+    private final PreAttestationParamService paramService;
 
-    public KafkaSadimService(@Qualifier("sadimStreamApiParser") SadimJsonParser sadimJsonParser) {
+    public KafkaSadimService(@Qualifier("sadimStreamApiParser") SadimJsonParser sadimJsonParser,
+                             PreAttestationParamService paramService) {
         this.sadimJsonParser = sadimJsonParser;
+        this.paramService = paramService;
     }
 
-    //    @KafkaListener(containerFactory = "kafkaListenerSadim",
-//            topicPartitions = {@TopicPartition(topic = "PA-MU.NLMK.P3.HSM",
-//            partitionOffsets = @PartitionOffset(partition = "0", initialOffset = "0")
-//            ),
-//            })
     @KafkaListener(containerFactory = "kafkaListenerSadim", topics = {"${kafka.sadim.topic}"})
-    public void receiveMessageReq(@Header(KafkaHeaders.RECEIVED_MESSAGE_KEY) String key,
-                                  @Payload ConsumerRecord consumerRecord
-    ) {
+    public void receiveMessageReq(@Payload ConsumerRecord consumerRecord) {
 
-        log.info("SADIM offset: {}, message key: {}", consumerRecord.offset(), key);
-
-        val attestationParam = sadimJsonParser.getParam(consumerRecord.value().toString());
-
+        Optional<PreAttestationParam> attestationParam = sadimJsonParser.getParam(consumerRecord.value().toString());
         log.info("--- SADIM message with offset: {}; attestationParam:{}", consumerRecord.offset(), attestationParam.get());
+
+        if (attestationParam.isPresent()) {
+            attestationParam = paramService.save(attestationParam.get());
+            log.info("--- SADIM param successfully saved. param.primeId:{} ", attestationParam.get().getPrimeId());
+        }
     }
 }
