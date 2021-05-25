862697db9cc78f6f5ea6bd2d884b805ea39366c9
Носов Сергей Владимирович (nosov_sv) <nosov_sv@nlmk.com>
2021-05-25 12:46:06 +0900

    изменение KbController::sadim - переключение на Dto

--- src/main/java/com/nlmk/kb/server/service/impl/DtoComverterImpl.java
+++ src/main/java/com/nlmk/kb/server/service/impl/DtoComverterImpl.java
@@ -7,8 +7,6 @@ import lombok.extern.slf4j.Slf4j;
 import lombok.val;
 import org.springframework.stereotype.Service;
 
-import java.util.Arrays;
-
 @Slf4j
 @Service
 public class DtoComverterImpl implements DtoConverter {
