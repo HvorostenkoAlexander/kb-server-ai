fdd9cc6bfd0b98991d462932b5b01dc5ea97934f
Aleksandr Vakhrameyev <avakhrameyev@softmedialab.com>
2021-06-07 14:50:27 +0500

    APCS-101 борьба с CORS в swagger-ui

--- src/main/java/com/nlmk/kb/server/config/SwaggerConfig.java
+++ src/main/java/com/nlmk/kb/server/config/SwaggerConfig.java
@@ -3,7 +3,6 @@ package com.nlmk.kb.server.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.security.*;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,13 +11,10 @@ import java.util.Collections;
 @Configuration
 public class SwaggerConfig {
 
-    @Value("${keycloak.auth-server-url}") String keycloakServerUrl;
-    @Value("${keycloak.realm}") String keycloakRealm;
-
     @Bean
     public OpenAPI customOpenAPI() {
 
-        var authUrl = String.format("%s/realms/%s/protocol/openid-connect", keycloakServerUrl, keycloakRealm);
+        var authUrl = "/auth";
 
         return new OpenAPI()
             .components(new Components()
@@ -27,7 +23,7 @@ public class SwaggerConfig {
                     .description("Oauth2 flow")
                     .flows(new OAuthFlows()
                         .clientCredentials(new OAuthFlow()
-                            .authorizationUrl(authUrl + "/auth")
+//                            .authorizationUrl(authUrl + "/auth")
                             .refreshUrl(authUrl + "/token")
                             .tokenUrl(authUrl + "/token")
                             .scopes(new Scopes())
---
+++ src/main/java/com/nlmk/kb/server/controller/AuthenticationController.java
@@ -0,0 +1,74 @@
+package com.nlmk.kb.server.controller;
+
+import com.nlmk.kb.server.exception.KeycloakAuthorizationException;
+import io.swagger.v3.oas.annotations.Hidden;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+import java.util.Base64;
+import java.util.Collections;
+
+@CrossOrigin(origins = "*", methods = {RequestMethod.OPTIONS, RequestMethod.POST, RequestMethod.GET})
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(path = "/auth")
+@Hidden
+public class AuthenticationController {
+
+    @Value("${keycloak.realm}") String realm;
+    private static final String TOKEN_URL_TEMPLATE = "/realms/%s/protocol/openid-connect/token";
+    private final WebClient keycloakClient;
+
+    @Getter
+    @Setter
+    public static class KeycloakResponse {
+        String token_type;
+        String access_token;
+        Integer expires_in;
+        String refresh_token;
+        Integer refresh_expires_in;
+    }
+
+    @PostMapping("/token")
+    public ResponseEntity getToken(@RequestHeader("Authorization") String authHeader) {
+
+        String[] authData =
+            new String(Base64.getDecoder().decode(authHeader.replace("Basic ", ""))).split(":");
+
+        String tokenUrl = String.format(TOKEN_URL_TEMPLATE, realm);
+
+        MultiValueMap<String, String> credentials = new LinkedMultiValueMap<>();
+        credentials.put("grant_type", Collections.singletonList("client_credentials"));
+        credentials.put("client_id", Collections.singletonList(authData[0]));
+        credentials.put("client_secret", Collections.singletonList(authData[1]));
+
+        try {
+            KeycloakResponse res = keycloakClient.post()
+                .uri(tokenUrl)
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .accept(MediaType.APPLICATION_JSON)
+                .body(BodyInserters.fromFormData(credentials))
+                .retrieve()
+                .onStatus(HttpStatus::is4xxClientError, response -> response
+                    .bodyToMono(String.class)
+                    .flatMap(error -> Mono.error(new KeycloakAuthorizationException(error))))
+                .bodyToMono(KeycloakResponse.class)
+                .block();
+
+            return ResponseEntity.ok().body(res);
+        } catch (KeycloakAuthorizationException e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.getMessage());
+        }
+    }
+}
