d381fa364daac1eb696955bbf838bd99d66e4e26
Носов Сергей Владимирович (nosov_sv) <nosov_sv@nlmk.com>
2021-06-30 10:35:46 +0900

    проверка поиска по primeId в сообщениях sadim

--- src/main/java/com/nlmk/kb/server/service/impl/SadimMessageServiceImpl.java
+++ src/main/java/com/nlmk/kb/server/service/impl/SadimMessageServiceImpl.java
@@ -57,7 +57,7 @@ public class SadimMessageServiceImpl implements SadimMessageService {
         List<SadimMessage> messagesCheck = sadimMessageRepository.findSadimMessagesByParam_PrimeIdOrderByTsDesc("0001020210630010959757133");
         log.info("messagesCheck size by primeId (0001020210630010959757133): {}", messagesCheck.size());
         messagesCheck.forEach(m->log.info("--- SadimMessage with primeId (0001020210630010959757133): {}",m));
-        log.info("--- message(0): {}",messages.get(0));
+        log.info("--- message(0): {}",messagesCheck.get(0));
 
         return messages;
     }
