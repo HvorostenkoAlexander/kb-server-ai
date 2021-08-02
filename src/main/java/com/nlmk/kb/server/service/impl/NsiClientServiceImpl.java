e05a3421f9c66da3bd622d04027a1a5879e414f3
Носов Сергей Владимирович (nosov_sv) <nosov_sv@nlmk.com>
2021-08-02 17:39:57 +0300

    проверка отправки PdmMessage

--- src/main/java/com/nlmk/kb/server/service/impl/NsiClientServiceImpl.java
+++ src/main/java/com/nlmk/kb/server/service/impl/NsiClientServiceImpl.java
@@ -4,6 +4,7 @@ import com.nlmk.kb.server.entity.pdm.PdmMessage;
 import com.nlmk.kb.server.service.MessageSender;
 import com.nlmk.kb.server.service.NsiClientService;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
@@ -26,11 +27,23 @@ public class NsiClientServiceImpl implements NsiClientService {
     @Override
     public ResponseEntity<Long> sendPdmMessage(PdmMessage message) {
 
+        // изменение названия топика с целью отправки сообщение полученных из тестового топика
+        setupTopicName(message);
+
         MessageSender sender = senders.get(message.getTopic());
 
-        if (sender==null){
-            throw new IllegalArgumentException("Не поддерживается отправка сообщений в nsi-server для топика: "+message.getTopic());
+        if (sender == null) {
+            throw new IllegalArgumentException("Не поддерживается отправка сообщений в nsi-server для топика: " + message.getTopic());
         }
         return sender.send(message);
+    }
+
+    // с целью отправки сообщение полученных из тестового топика
+    private void setupTopicName(PdmMessage message){
+        if (StringUtils.startsWith(message.getTopic(), "000-1.")) {
+            message.setTopic(
+                    StringUtils.replaceOnce(message.getTopic(),"000-1.","000-0.")
+            );
+        }
     }
 }
