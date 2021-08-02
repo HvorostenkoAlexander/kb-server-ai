716ff64290c639d2fe544499ed65a07e821c41a3
Носов Сергей Владимирович (nosov_sv) <nosov_sv@nlmk.com>
2021-08-02 13:44:49 +0300

    выделение чтения топика kafka.pdm.topic.microstructure в отдельный метод для проверки внесенных изменений

--- src/main/java/com/nlmk/kb/server/service/KafkaPdmService.java
+++ src/main/java/com/nlmk/kb/server/service/KafkaPdmService.java
@@ -8,6 +8,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.PartitionOffset;
+import org.springframework.kafka.annotation.TopicPartition;
 import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
@@ -24,7 +26,7 @@ public class KafkaPdmService {
 
     @KafkaListener(containerFactory = "kafkaListenerContainerFactoryPdm",
             topics = {
-                    "${kafka.pdm.topic.microstructure}",
+//                    "${kafka.pdm.topic.microstructure}",
                     "${kafka.pdm.topic.asap-chemical-properties}",
                     "${kafka.pdm.topic.equivalents}",
                     "${kafka.pdm.topic.match-tk-num}",
@@ -45,7 +47,19 @@ public class KafkaPdmService {
     )
     @Timed(value = "kafka_listener", percentiles = {0.99, 0.95})
     public void receiveMessageReq(@Payload ConsumerRecord request, Acknowledgment ack) {
+        messageHendler(request, ack);
+    }
 
+    @KafkaListener(containerFactory = "kafkaListenerContainerFactoryPdm",
+            topicPartitions = {
+                    @TopicPartition(topic = "${kafka.pdm.topic.microstructure}",
+                            partitionOffsets = @PartitionOffset(partition = "0,1", initialOffset = "0"))})
+    @Timed(value = "kafka_listener", percentiles = {0.99, 0.95})
+    public void receiveMessage1(@Payload ConsumerRecord request, Acknowledgment ack) {
+        messageHendler(request, ack);
+    }
+
+    private void messageHendler(@Payload ConsumerRecord request, Acknowledgment ack) {
         log.info("PDM consumer record: topic: {}; partition: {}; offset: {}, key: {}",
                 request.topic(),
                 request.partition(),
