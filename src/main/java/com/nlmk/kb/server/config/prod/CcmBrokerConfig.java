954c1868522acd16e566c76e1ce8c983bd840f10
Носов Сергей Владимирович (nosov_sv) <nosov_sv@nlmk.com>
2021-08-04 10:02:03 +0300

    настройка переключения prod/test для CCM

--- src/main/java/com/nlmk/kb/server/config/prod/CcmBrokerConfig.java
+++ src/main/java/com/nlmk/kb/server/config/prod/CcmBrokerConfig.java
@@ -37,12 +37,16 @@ public class CcmBrokerConfig {
         props.put("schema.registry.url", consumerProperties.getSchemaRegistryUrl());
         props.put("specific.avro.reader", "true");
 
-        props.put("security.protocol", "SSL");
-        props.put("ssl.truststore.location", "kafkaSsl/client.truststore.jks");
-        props.put("ssl.truststore.password", "assagai");
-        props.put("ssl.keystore.password", "assagai");
-        props.put("ssl.keystore.location", "kafkaSsl/client.keystore.jks");
-        props.put("ssl.endpoint.identification.algorithm", "");
+        if (consumerProperties.isProductionTopic()) {
+            log.warn("Внимание! Подключаются настройки для продуктового топика ССМ");
+
+            props.put("security.protocol", "SSL");
+            props.put("ssl.truststore.location", consumerProperties.getSslTruststorePath());
+            props.put("ssl.truststore.password", consumerProperties.getSslTruststorePassword());
+            props.put("ssl.keystore.password", consumerProperties.getSslKeystorePassword());
+            props.put("ssl.keystore.location", consumerProperties.getSslKeystorePath());
+            props.put("ssl.endpoint.identification.algorithm", "");
+        }
 
         return props;
     }
