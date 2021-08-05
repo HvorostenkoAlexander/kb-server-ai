30ac97612ec2b62d7160b8e76e683b0c83951348
Носов Сергей Владимирович (nosov_sv) <nosov_sv@nlmk.com>
2021-08-05 13:33:47 +0300

    изменение параметров ssl

--- src/main/java/com/nlmk/kb/server/KbServerApplication.java
+++ src/main/java/com/nlmk/kb/server/KbServerApplication.java
@@ -2,6 +2,8 @@ package com.nlmk.kb.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.StandardEnvironment;
 import org.springframework.kafka.annotation.EnableKafka;
 
 @EnableKafka
@@ -9,14 +11,14 @@ import org.springframework.kafka.annotation.EnableKafka;
 public class KbServerApplication {
 
     public static void main(String[] args) {
-         SpringApplication.run(KbServerApplication.class, args);
+//         SpringApplication.run(KbServerApplication.class, args);
 
-//        SpringApplication application = new SpringApplication(KbServerApplication.class);
-//        ConfigurableEnvironment environment = new StandardEnvironment();
-//        environment.setActiveProfiles("prod");
-//        application.setEnvironment(environment);
-//
-//        application.run(args);
+        SpringApplication application = new SpringApplication(KbServerApplication.class);
+        ConfigurableEnvironment environment = new StandardEnvironment();
+        environment.setActiveProfiles("prod");
+        application.setEnvironment(environment);
+
+        application.run(args);
     }
 
 }
