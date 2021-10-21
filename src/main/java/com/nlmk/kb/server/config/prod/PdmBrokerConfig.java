209a6d3f1240c1666d73a99ec9c7d88d03be6a87
Носов Сергей Владимирович (nosov_sv) <nosov_sv@nlmk.com>
2021-10-21 11:05:34 +0300

    настройка файлов properties для профилей dev, prod и для тестирования

--- src/main/java/com/nlmk/kb/server/config/prod/PdmBrokerConfig.java
+++ src/main/java/com/nlmk/kb/server/config/prod/PdmBrokerConfig.java
@@ -27,6 +27,7 @@ public class PdmBrokerConfig {
     private final PdmConsumerProperties consumerProperties;
 
     @Bean
+    @Primary
     public ConsumerFactory<Object, Object> pdmConsumerFactory() {
         Map<String, Object> props = new HashMap<>();
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, consumerProperties.getKafkaServer());
