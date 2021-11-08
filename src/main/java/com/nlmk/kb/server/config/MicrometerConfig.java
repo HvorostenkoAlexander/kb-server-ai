95010b4af2ba59c163d78aec29fb637f3960ff6b
Носов Сергей Владимирович (nosov_sv) <nosov_sv@nlmk.com>
2021-11-08 14:21:27 +0300

    отключение логирования в  MicrometerConfig::timedAspect

--- src/main/java/com/nlmk/kb/server/config/MicrometerConfig.java
+++ src/main/java/com/nlmk/kb/server/config/MicrometerConfig.java
@@ -2,19 +2,14 @@ package com.nlmk.kb.server.config;
 
 import io.micrometer.core.aop.TimedAspect;
 import io.micrometer.core.instrument.MeterRegistry;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Slf4j
 @Configuration
 public class MicrometerConfig {
 
     @Bean
     public TimedAspect timedAspect(MeterRegistry registry) {
-
-        log.info("--- timedAspect: MeterRegistry.config: [{}]", registry.config());
-
         return new TimedAspect(registry);
     }
 }
