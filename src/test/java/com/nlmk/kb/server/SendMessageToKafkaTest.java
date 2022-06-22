9ae42ab696fd9f1e757c290e718c675bba72e009
Dmitrii V. Lystsov <demetriuzz@yandex.ru>
2022-06-22 13:49:46 +0500

    SendMessageToKafkaTest update, add simple zordersTestVariant.xml

--- src/test/java/com/nlmk/kb/server/SendMessageToKafkaTest.java
+++ src/test/java/com/nlmk/kb/server/SendMessageToKafkaTest.java
@@ -18,12 +18,12 @@ import java.util.Properties;
 class SendMessageToKafkaTest {
 
     KafkaProducer<Object, Object> stringProducer;
-    private final String sadimTopic = "PA-MU.NLMK.P3.HSM";
+    private static final String SADIM_TOPIC = "PA-MU.NLMK.P3.HSM";
     private final ObjectMapper mapper = new ObjectMapper();
 
     KafkaProducer<Object, Object> avroProducer;
     // одна комбинация: тема + схема (версия схемы привязана к теме!)
-    private final String ccmTopic = "000-1.l3-ccm-pgp.db.Attestation-Request.0";
+    private static final String CCM_TOPIC = "000-1.l3-ccm-pgp.db.Attestation-Request.0";
 
     @BeforeAll
     void setUp() {
@@ -71,16 +71,16 @@ class SendMessageToKafkaTest {
         // nlmk.sadim.Sadim класс составленный на основе примеров сообщений САДиМ, полученных как JSON.
         final var value = new Sadim();
         final var strip = new Strip();
-        strip.setTimeRolling(new Date(1_000_000_000L));
-        strip.setPrimeId("pi100");
-        strip.setT12Min(1.0);
-        strip.setT12Max(2.0);
+        strip.setTimeRolling(new Date(1_655_880_000_000L));
+        strip.setPrimeId("0001020210329001515440422");
+        strip.setT12Min(795.0);
+        strip.setT12Max(835.0);
         value.setStrips(List.of(strip));
-        value.setLotNo(12);
-        value.setMeltNo(13);
+        value.setLotNo(25217); // -> hnum
+        value.setMeltNo(2106684); // -> nplv
 
         ProducerRecord<Object, Object> record = new ProducerRecord<>(
-                sadimTopic,
+                SADIM_TOPIC,
                 // при наличие value key не анализируется
                 "key~" + Instant.now().getEpochSecond(),
                 mapper.writeValueAsString(value)
@@ -92,28 +92,47 @@ class SendMessageToKafkaTest {
     @Test
     void sendCcmMessage() {
         nlmk.l3.ccm.pgp.RecordData data = nlmk.l3.ccm.pgp.RecordData.newBuilder()
-                .setPrimeId("prime12")
+                .setPrimeId("0001020210329001515440422")
+                .setNplv(2106684) // <- meltNo
+                .setHnum(25217) // <-- lotNo
+                .setRoll("1-1")
                 .setOrderPos(1)
-                .setRoll("r2")
-                .setThickness(1.2f)
-                .setWidth(1.3f)
-                .setWeightNet(1.4f)
-                .setKceh(3)
-                .setSpecifications(List.of())
+                .setThickness(2.65f)
+                .setWidth(1232.0f)
+                .setWeightNet(10.86f)
+                .setKceh(12)
+                .setOrderNum(40434341L)
+                .setOrderPos(4)
+                // спецификация
+                .setSpecifications(List.of(
+                        nlmk.l3.ccm.pgp.RecordSpecifications.newBuilder().setSpecTypeCode(1)
+                                .setSpecCode(3).setSpecName("Марка стали").setSpecValue("Ст3сп").build(),
+                        nlmk.l3.ccm.pgp.RecordSpecifications.newBuilder().setSpecTypeCode(2)
+                                .setSpecCode(45).setSpecName("Признак травления").setSpecValue("0").build()
+                ))
+                // ТрЗак
+                .setOrderReq(List.of(
+                        nlmk.l3.ccm.pgp.RecordOrderReq.newBuilder().setAttrTypeCode(1).setAttrTypeValue(1)
+                                .setAttrCode(219).setAttrName("Наименование продукции")
+                                .setAttrValue("Сталь тонколистовая горячекатаная").build(),
+                        nlmk.l3.ccm.pgp.RecordOrderReq.newBuilder().setAttrTypeCode(1).setAttrTypeValue(1)
+                                .setAttrCode(4).setAttrName("Вид продукции")
+                                .setAttrValue("РУЛОН").build()
+                ))
                 .build();
 
         nlmk.l3.ccm.pgp.AttestationRequest value = nlmk.l3.ccm.pgp.AttestationRequest.newBuilder()
-                .setTs("2021-04-08T10:35:25.452-03:00")
-                .setOp(nlmk.l3.ccm.pgp.EnumOp.I)
+                .setTs("2021-05-18T00:38:25.194-03:00")
+                .setOp(nlmk.l3.ccm.pgp.EnumOp.U)
                 .setPk(nlmk.l3.ccm.pgp.RecordPk.newBuilder()
-                        .setId("0001020111023170548152095")
+                        .setId("0001020210329001515440422")
                         .setSystemCode("16")
                         .build())
                 .setData(data)
                 .build();
 
         ProducerRecord<Object, Object> record = new ProducerRecord<>(
-                ccmTopic,
+                CCM_TOPIC,
                 "key~" + Instant.now().getEpochSecond(), // случайный key
                 value
         );
