36d164e7ec1c98109eba92444928ba3c7a3200ae
Dmitrii V. Lystsov <demetriuzz@yandex.ru>
2022-07-01 15:08:14 +0500

    AttestationMessage, entity, repository, test

---
+++ src/main/java/com/nlmk/kb/server/repository/AttestationMessageRepository.java
@@ -0,0 +1,18 @@
+package com.nlmk.kb.server.repository;
+
+import com.nlmk.kb.server.entity.AttestationMessage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface AttestationMessageRepository extends JpaRepository<AttestationMessage, Long> {
+
+    /**
+     * Найти последнее принятое сообщение
+     *
+     * @param primeId значение фильтра, идентификатор единицы металла
+     * @return сообщение, если есть
+     */
+    Optional<AttestationMessage> findFirstByPrimeIdOrderByReceiptTsDesc(String primeId);
+
+}
