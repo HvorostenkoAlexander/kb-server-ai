e3fd90d659ba759c6d6fe7893d6ef1ec682bdd5d
Dmitrii V. Lystsov <demetriuzz@yandex.ru>
2022-07-04 14:59:29 +0500

    CcmPtsRequest, add fields, test

--- src/test/java/com/nlmk/kb/server/controller/AttestationControllerTest.java
+++ src/test/java/com/nlmk/kb/server/controller/AttestationControllerTest.java
@@ -12,6 +12,8 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import java.util.List;
+
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(AttestationController.class)
@@ -34,10 +36,81 @@ class AttestationControllerTest {
         mvc.perform(MockMvcRequestBuilders.post("/attestation/ccm/pts")
                         .header(HttpHeaders.AUTHORIZATION, "T V")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(mapper.writeValueAsString(
-                                CcmPtsRequest.builder().ts("2022-07-01T00:00:00.000Z").build()
-                        )))
+                        .content(prepareMinimalRequest()))
                 .andExpect(status().isCreated());
+
+        mvc.perform(MockMvcRequestBuilders.post("/attestation/ccm/pts")
+                        .header(HttpHeaders.AUTHORIZATION, "T V")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(prepareRequestWithMinimalData()))
+                .andExpect(status().isCreated());
+    }
+
+    private String prepareMinimalRequest() throws Exception {
+        return mapper.writeValueAsString(
+                CcmPtsRequest.builder()
+                        .ts("2022-07-01T00:00:00.000Z")
+                        .pk(CcmPtsRequest.Pk.builder().systemCode("sc").id("id").build())
+                        .build()
+        );
+    }
+
+    private String prepareRequestWithMinimalData() throws Exception {
+        return mapper.writeValueAsString(
+                CcmPtsRequest.builder()
+                        .ts("2022-07-01T00:00:00.000Z")
+                        .pk(CcmPtsRequest.Pk.builder().systemCode("sc").id("id").build())
+                        .data(CcmPtsRequest.Record.builder()
+                                .werks(10).werksName("w10")
+                                .kceh(11).kcehName("k11")
+                                .unitCode(12).unitName("u12")
+                                .storageCode(13).storageName("s13")
+                                .marking(CcmPtsRequest.Marking.builder().nplv(20).hnum(21).tnum(22).roll(23).build())
+                                .weightNet(14.0)
+                                .geometry(CcmPtsRequest.Geometry.builder().thickness(30.0).width(32.0).length(31.0).build())
+                                .bundles(List.of(
+                                        CcmPtsRequest.Bundle.builder().stripId("s40").stripNum(41)
+                                                .stripWidth(42.0).stripWeight(43.0).build()
+                                ))
+                                .specifications(List.of(
+                                        CcmPtsRequest.Specification.builder()
+                                                .specCode(50).specName("s51")
+                                                .specTypeCode(52).specTypeName("s53")
+                                                .specTypeValue(1).listValues(List.of(
+                                                        CcmPtsRequest.OneSpecValue.builder().value("v54").build()
+                                                )).build()
+                                ))
+                                .properties(List.of(
+                                        CcmPtsRequest.OneProperty.builder().typeCode(60).typeName("t61")
+                                                .analyzes(List.of(
+                                                        CcmPtsRequest.OnePropAnalyze.builder()
+                                                                .samplingPlaceCode(62).samplingPlaceName("s63")
+                                                                .analysisValue(2)
+                                                                .listValues(List.of(
+                                                                        CcmPtsRequest.OnePropValue.builder()
+                                                                                .attrCode(64).attrType(3).build()
+                                                                )).build()
+                                                ))
+                                                .attestationList(List.of(
+                                                        CcmPtsRequest.OnePropAtt.builder()
+                                                                .typeCode(70).typeName("t71")
+                                                                .listValues(List.of(
+                                                                        CcmPtsRequest.OneAttValue.builder()
+                                                                                .side(72).attrCode(73).attrValue(74.0)
+                                                                                .build()
+                                                                )).build()
+                                                )).build()
+                                ))
+                                .chemical(List.of(
+                                        CcmPtsRequest.Chemical.builder()
+                                                .id(80).listValues(List.of(
+                                                        CcmPtsRequest.OneChemicalValue.builder()
+                                                                .code(81).name("n82").build()
+                                                )).build()
+                                ))
+                                .build())
+                        .build()
+        );
     }
 
 }
