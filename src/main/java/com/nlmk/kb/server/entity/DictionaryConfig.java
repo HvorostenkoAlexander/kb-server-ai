137d72a54ce0c24545bd5a13605f497490cc8ce1
Dmitrii V. Lystsov <demetriuzz@yandex.ru>
2022-07-22 18:43:26 +0500

    #896 fill dictionary_config for new topic, test

--- src/main/java/com/nlmk/kb/server/entity/DictionaryConfig.java
+++ src/main/java/com/nlmk/kb/server/entity/DictionaryConfig.java
@@ -7,14 +7,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
-import javax.persistence.Column;
-import javax.persistence.ElementCollection;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.util.List;
 
 @Getter
@@ -36,6 +29,7 @@ public class DictionaryConfig {
     private String topic; // имя топика
 
     @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name = "dictionary_config_codes", joinColumns = @JoinColumn(name = "dictionary_config_id"))
     private List<Integer> codes; // перечень характеристик(кодов)
 
     @Column(nullable = false, name = "nsi_path")
--- src/test/java/com/nlmk/kb/server/repository/DictionaryConfigRepositoryTest.java
+++ src/test/java/com/nlmk/kb/server/repository/DictionaryConfigRepositoryTest.java
@@ -4,6 +4,9 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.jdbc.Sql;
+
+import java.util.List;
 
 @DataJpaTest
 class DictionaryConfigRepositoryTest {
@@ -12,8 +15,21 @@ class DictionaryConfigRepositoryTest {
     private DictionaryConfigRepository repository;
 
     @Test
-    void init() {
-        Assertions.assertEquals(0L, repository.count());
+    @Sql(scripts = "classpath:/sql/dictionary_config.sql")
+    void findByTopic() {
+        Assertions.assertEquals(2L, repository.count());
+
+        Assertions.assertTrue(repository.findByTopic("t1").isEmpty());
+
+        final var topic1 = repository.findByTopic("topic.pdm.dict-1.0");
+        Assertions.assertTrue(topic1.isPresent());
+        Assertions.assertTrue(topic1.get().getEnabled());
+        Assertions.assertEquals(List.of(2,3,138,333), topic1.get().getCodes());
+
+        final var topic2 = repository.findByTopic("topic.pdm.dict-2.0");
+        Assertions.assertTrue(topic2.isPresent());
+        Assertions.assertFalse(topic2.get().getEnabled());
+        Assertions.assertEquals(List.of(1,3), topic2.get().getCodes());
     }
 
 }
