0e167ed85ddeb4e041bdf2a75b3e92c4525aec58
Dmitrii V. Lystsov <demetriuzz@yandex.ru>
2022-09-15 10:07:58 +0500

    #976 remove RestTemplate

--- src/main/java/com/nlmk/kb/server/service/result/sending/ResultSenderImpl.java
+++ src/main/java/com/nlmk/kb/server/service/result/sending/ResultSenderImpl.java
@@ -1,6 +1,5 @@
 package com.nlmk.kb.server.service.result.sending;
 
-import com.fasterxml.jackson.databind.JsonNode;
 import com.nlmk.kb.server.api.MessagesBatchDto;
 import com.nlmk.kb.server.entity.KafkaMessageKey;
 import com.nlmk.kb.server.exception.KafkaRestConfigException;
@@ -15,19 +14,13 @@ import nlmk.l3.apcs.VerificationResultsPts;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.*;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.stereotype.Service;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
 import java.nio.charset.StandardCharsets;
 import java.time.Duration;
-import java.util.Collections;
 
 import static com.nlmk.kb.server.config.KbConstants.*;
 
@@ -40,32 +33,14 @@ public class ResultSenderImpl implements ResultSenderPgp, ResultSenderPts {
     private final WebClient webClient;
     private final int webClientTimeout;
 
-    private final RestTemplate restTemplate;
-    private static final Long DEFAULT_CONNECT_TIMEOUT = 30000L;
-    private static final Long DEFAULT_READ_TIMEOUT = 30000L;
-
     public ResultSenderImpl(@Value("${service-web-client.kafka-rest.address}") String kafkaHttpProxyAddress,
-                            @Value("${service-web-client.kafka-rest.login}") String kafkaHttpProxyLogin,
-                            @Value("${service-web-client.kafka-rest.password}") String kafkaHttpProxyPassword,
                             @Value("${service-web-client.timeout:5000}") int timeout,
                             @Qualifier("basicAuthWebClient") WebClient webClient,
-                            KafkaRestMessageAdapter kafkaRestMessageAdapter,
-                            RestTemplateBuilder restTemplateBuilder) {
+                            KafkaRestMessageAdapter kafkaRestMessageAdapter) {
         this.webClient = webClient;
         this.webClientTimeout = timeout;
         this.kafkaHttpProxyAddress = kafkaHttpProxyAddress;
         this.kafkaRestMessageAdapter = kafkaRestMessageAdapter;
-
-        if (org.springframework.util.StringUtils.hasText(kafkaHttpProxyLogin)
-                && org.springframework.util.StringUtils.hasText(kafkaHttpProxyPassword)) {
-            restTemplateBuilder = restTemplateBuilder.basicAuthentication(kafkaHttpProxyLogin, kafkaHttpProxyPassword);
-        }
-
-        restTemplate = restTemplateBuilder
-                .messageConverters(new MappingJackson2HttpMessageConverter())
-                .setConnectTimeout(Duration.ofMillis(DEFAULT_CONNECT_TIMEOUT))
-                .setReadTimeout(Duration.ofMillis(DEFAULT_READ_TIMEOUT))
-                .build();
     }
 
     private void checkBeforeSend(Object result, String topic) {
@@ -127,29 +102,6 @@ public class ResultSenderImpl implements ResultSenderPgp, ResultSenderPts {
                 })
                 .block();
         log.info("sending, response from KAFKA: [{}]", response);
-    }
-
-    private void sendingRestTemplate(MessagesBatchDto batchDto, String topic) {
-        try {
-            ResponseEntity<JsonNode> resp = restTemplate.exchange(
-                    String.format(KAFKA_REST_PROXY_TEMPLATE, kafkaHttpProxyAddress, topic),
-                    HttpMethod.POST,
-                    buildHttpEntity(batchDto),
-                    JsonNode.class
-            );
-
-            log.info("sending, response from KAFKA: [{}]", resp);
-        } catch (Exception e) {
-            log.error("sending, sending error: [{}]", e.getMessage());
-            throw new KafkaRestException(e);
-        }
-    }
-
-    private HttpEntity<MessagesBatchDto> buildHttpEntity(MessagesBatchDto batchDto) {
-        MultiValueMap<String, String> headers = new LinkedMultiValueMap<>();
-        headers.put(HttpHeaders.ACCEPT, Collections.singletonList(KAFKA_REST_ACCEPT_HEADER));
-        headers.put(HttpHeaders.CONTENT_TYPE, Collections.singletonList(KAFKA_REST_CONTENT_TYPE_HEADER));
-        return new HttpEntity<>(batchDto, headers);
     }
 
 }
