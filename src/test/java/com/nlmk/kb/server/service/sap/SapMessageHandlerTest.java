ea1f34f30b7b3775e06600f343920abfd4bf3324
Dmitrii V. Lystsov <demetriuzz@yandex.ru>
2022-10-06 16:07:17 +0500

    #1150 SapMessageHandlerImpl, clean order value, test

--- src/test/java/com/nlmk/kb/server/service/sap/SapMessageHandlerTest.java
+++ src/test/java/com/nlmk/kb/server/service/sap/SapMessageHandlerTest.java
@@ -1,6 +1,7 @@
 package com.nlmk.kb.server.service.sap;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.nlmk.attestation.product.api.specification.SapOrderPosCode;
 import com.nlmk.attestation.zorder.ZORDERS051;
 import com.nlmk.kb.server.entity.SapMessage;
 import com.nlmk.kb.server.entity.SapMessageState;
@@ -19,15 +20,19 @@ import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.client.HttpClientErrorException;
 
+import javax.xml.bind.JAXBException;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.text.MessageFormat;
 import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.never;
-import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.reset;
 
 @SpringBootTest
 class SapMessageHandlerTest {
@@ -56,14 +61,16 @@ class SapMessageHandlerTest {
                 .setTs("1652872288900")
                 .build();
 
-        ConsumerRecord<String, s3notification> consumerRecord = new ConsumerRecord<>("topic", 1, 0, null, kafkaMessage);
+        ConsumerRecord<String, s3notification> consumerRecord = new ConsumerRecord<>(
+                "topic", 1, 0, null, kafkaMessage
+        );
 
         final String s3zordersExample = Arrays.toString(new ClassPathResource("xml/zordersExample.xml").getInputStream().readAllBytes());
         final ZORDERS051 parsedZorder = objectMapper.readValue(new ClassPathResource("json/zordersExample.json").getFile(), ZORDERS051.class);
 
         // ошибка получения файла из S3 хранилища
 
-        Mockito.when(s3Service.getObjectAsString(any(), any())).thenThrow(new S3ClientException("---"));
+        when(s3Service.getObjectAsString(any(), any())).thenThrow(new S3ClientException("---"));
 
         var res1 = sapMessageHandler.handleConsumerRecord(consumerRecord);
 
@@ -76,10 +83,10 @@ class SapMessageHandlerTest {
 
         // файла из S3 хранилища получен, но некорректный
 
-        Mockito.reset(s3Service);
+        reset(s3Service);
 
-        Mockito.when(s3Service.getObjectAsString(any(), any())).thenReturn(s3zordersExample);
-        Mockito.when(s3Service.getZorder(any())).thenThrow(new S3ClientException("---"));
+        when(s3Service.getObjectAsString(any(), any())).thenReturn(s3zordersExample);
+        when(s3Service.getZorder(any())).thenThrow(new S3ClientException("---"));
 
         var res2 = sapMessageHandler.handleConsumerRecord(consumerRecord);
 
@@ -90,14 +97,14 @@ class SapMessageHandlerTest {
         assertNotNull(sapMessage2.getOrder());
         assertEquals(SapMessageState.ERROR, sapMessage2.getState());
 
-        Mockito.verify(psmSender, never()).postZorder(any());
+        verify(psmSender, never()).postZorder(any());
 
         // уведомления в статусах ERROR повторно не обрабатываются
 
-        Mockito.reset(s3Service);
+        reset(s3Service);
 
-        Mockito.when(s3Service.getObjectAsString(any(), any())).thenThrow(new S3ClientException("---"));
-        Mockito.when(s3Service.getZorder(any())).thenThrow(new S3ClientException("---"));
+        when(s3Service.getObjectAsString(any(), any())).thenThrow(new S3ClientException("---"));
+        when(s3Service.getZorder(any())).thenThrow(new S3ClientException("---"));
 
         var res3 = sapMessageHandler.handleConsumerRecord(consumerRecord);
 
@@ -108,16 +115,16 @@ class SapMessageHandlerTest {
         assertNotNull(sapMessage3.getOrder());
         assertEquals(SapMessageState.ERROR, sapMessage3.getState());
 
-        Mockito.verify(psmSender, never()).postZorder(any());
+        verify(psmSender, never()).postZorder(any());
 
         // ошибка передачи заказа в psm
 
         sapMessageRepository.deleteAll();
-        Mockito.reset(s3Service);
+        reset(s3Service);
 
-        Mockito.when(s3Service.getObjectAsString(any(), any())).thenReturn(s3zordersExample);
-        Mockito.when(s3Service.getZorder(any())).thenReturn(parsedZorder);
-        Mockito.when(psmSender.postZorder(any(ZORDERS051.class))).thenThrow(new HttpClientErrorException(HttpStatus.BAD_REQUEST));
+        when(s3Service.getObjectAsString(any(), any())).thenReturn(s3zordersExample);
+        when(s3Service.getZorder(any())).thenReturn(parsedZorder);
+        when(psmSender.postZorder(any(ZORDERS051.class))).thenThrow(new HttpClientErrorException(HttpStatus.BAD_REQUEST));
 
         var res4 = sapMessageHandler.handleConsumerRecord(consumerRecord);
 
@@ -128,16 +135,16 @@ class SapMessageHandlerTest {
         assertNotNull(sapMessage4.getOrder());
         assertEquals(SapMessageState.NEW, sapMessage4.getState());
 
-        Mockito.verify(psmSender, times(1)).postZorder(any(ZORDERS051.class));
+        verify(psmSender, times(1)).postZorder(any(ZORDERS051.class));
 
         // заказ передан в psm
 
-        Mockito.reset(s3Service);
-        Mockito.reset(psmSender);
+        reset(s3Service);
+        reset(psmSender);
 
-        Mockito.when(s3Service.getObjectAsString(any(), any())).thenReturn(s3zordersExample);
-        Mockito.when(s3Service.getZorder(any())).thenReturn(parsedZorder);
-        Mockito.when(psmSender.postZorder(any(ZORDERS051.class))).thenReturn(10);
+        when(s3Service.getObjectAsString(any(), any())).thenReturn(s3zordersExample);
+        when(s3Service.getZorder(any())).thenReturn(parsedZorder);
+        when(psmSender.postZorder(any(ZORDERS051.class))).thenReturn(10);
 
         var res5 = sapMessageHandler.handleConsumerRecord(consumerRecord);
 
@@ -149,9 +156,104 @@ class SapMessageHandlerTest {
         assertEquals(SapMessageState.DONE, sapMessage5.getState());
 
         final ArgumentCaptor<ZORDERS051> captor5 = ArgumentCaptor.forClass(ZORDERS051.class);
-        Mockito.verify(psmSender, times(1)).postZorder(captor5.capture());
+        verify(psmSender, times(1)).postZorder(captor5.capture());
 
         assertEquals("0040452892", captor5.getValue().getIDOC().getE1EDK01().getBELNR());
+
+        // почистить
+        sapMessageRepository.deleteAll();
+    }
+
+    @Test
+    void garbageCleaningTest() throws IOException, JAXBException {
+        S3Service localService = new S3ServiceImpl(null);
+
+        s3notification kafkaMessage = s3notification.newBuilder()
+                .setStorageType("s3")
+                .setServer("s3.xxx.com")
+                .setPath("zordersTestVariant.xml")
+                .setBucket("test bucket")
+                .setProcessorVersion("1.0")
+                .setTs("1652872288900")
+                .build();
+
+        ConsumerRecord<String, s3notification> consumerRecord = new ConsumerRecord<>(
+                "topic", 1, 0, null, kafkaMessage
+        );
+
+        final String xmlZOrder = new String(Files.readAllBytes(Path.of("src/test/resources/xml/zordersTestVariant.xml")));
+        ZORDERS051 zorder = localService.getZorder(xmlZOrder);
+
+        when(s3Service.getObjectAsString(any(), any())).thenReturn(xmlZOrder);
+        when(s3Service.getZorder(any())).thenReturn(zorder);
+        when(psmSender.postZorder(any(ZORDERS051.class))).thenReturn(100);
+
+        var res = sapMessageHandler.handleConsumerRecord(consumerRecord);
+        assertTrue(res);
+
+        ArgumentCaptor<ZORDERS051> zorderCaptor = ArgumentCaptor.forClass(ZORDERS051.class);
+        verify(psmSender, times(1)).postZorder(zorderCaptor.capture());
+
+        final ZORDERS051 zorderSended = zorderCaptor.getValue();
+        assertEquals("40434341", zorderSended.getIDOC().getE1EDK01().getBELNR());
+
+        zorderSended.getIDOC().getE1CUCFG().forEach(
+                e1cucfg -> e1cucfg.getE1CUVAL().forEach(
+                        e1cuval -> {
+                            // все коды, которые есть в заказе
+                            SapOrderPosCode code = SapOrderPosCode.valueOf(e1cuval.getCHARC());
+                            switch (code) {
+                                case STNDRT_PROD:
+                                case STNDRT_MARKA:
+                                case STNDRT_SORT:
+                                    assertEquals("ГОСТ 14918-2020", e1cuval.getVALUE());
+                                    break;
+                                case CEH_PROD:
+                                    assertEquals("11", e1cuval.getVALUE());
+                                    break;
+                                case MARKA:
+                                    assertEquals("02", e1cuval.getVALUE());
+                                    break;
+                                case VID_POSTAVKI:
+                                    assertEquals("РЛН", e1cuval.getVALUE());
+                                    break;
+                                case KROM:
+                                    assertEquals("НО", e1cuval.getVALUE());
+                                    break;
+                                case TPRK:
+                                    assertEquals("ТУ 0027", e1cuval.getVALUE());
+                                    break;
+                                case TEXK:
+                                case RABPL:
+                                case GROT:
+                                case ROUTE_TK:
+                                    assertNull(e1cuval.getVALUE(),
+                                            MessageFormat.format("not NULL value in code {0}", code));
+                                    break;
+                                case SHOT_MIN:
+                                    assertEquals("1250.0", e1cuval.getVALUE());
+                                    break;
+                                case SHOT_MAX:
+                                    assertEquals("1234567.8", e1cuval.getVALUE());
+                                    break;
+                                case DLIN_MIN:
+                                    assertEquals("6000", e1cuval.getVALUE());
+                                    break;
+                                case DLIN_MAX:
+                                    assertEquals("3000.0", e1cuval.getVALUE());
+                                    break;
+                                case VN_DIAM_RL:
+                                    assertEquals("600", e1cuval.getVALUE());
+                                    break;
+                            }
+                        }
+                )
+        );
+
+        reset(s3Service);
+        reset(psmSender);
+        // почистить
+        sapMessageRepository.deleteAll();
     }
 
 }
