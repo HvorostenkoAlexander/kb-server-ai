2a31e082431c7e526ca0cab81c0013dbdd67f1aa
Aleksandr Vakhrameyev <avakhrameyev@softmedialab.com>
2022-12-21 17:14:22 +0500

    refactoring Broker Configs

---
+++ src/main/java/com/nlmk/kb/server/config/broker/BrokerConfigBase.java
@@ -0,0 +1,72 @@
+package com.nlmk.kb.server.config.broker;
+
+import io.confluent.kafka.serializers.KafkaAvroDeserializer;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.listener.ContainerProperties;
+import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
+abstract class BrokerConfigBase {
+
+    private ConsumerFactory<Object, Object> getConsumerFactory(ConsumerProperties consumerProperties) {
+        Map<String, Object> configuration = new HashMap<>();
+
+        configuration.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, consumerProperties.getKafkaServer());
+        configuration.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, KafkaAvroDeserializer.class);
+        configuration.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, KafkaAvroDeserializer.class);
+        configuration.put(ConsumerConfig.GROUP_ID_CONFIG, consumerProperties.getKafkaGroupId());
+        configuration.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        configuration.put("schema.registry.url", consumerProperties.getSchemaRegistryUrl());
+        configuration.put("specific.avro.reader", "true");
+
+        if (consumerProperties.isSslEnabled()) {
+            configuration.put("security.protocol", "SSL");
+            configuration.put("ssl.truststore.location", consumerProperties.getTruststorePath());
+            configuration.put("ssl.truststore.password", consumerProperties.getTruststorePassword());
+            configuration.put("ssl.keystore.password", consumerProperties.getKeystorePassword());
+            configuration.put("ssl.keystore.location", consumerProperties.getKeystorePath());
+            configuration.put("ssl.endpoint.identification.algorithm", "");
+        }
+
+        KafkaAvroDeserializer keyDeserializer = new KafkaAvroDeserializer();
+        keyDeserializer.configure(configuration, true);
+
+        KafkaAvroDeserializer valueDeserializer = new KafkaAvroDeserializer();
+        valueDeserializer.configure(configuration, false);
+
+        ErrorHandlingDeserializer<Object> errorHandlingKeyDeserializer
+                = new ErrorHandlingDeserializer<>(keyDeserializer);
+
+        ErrorHandlingDeserializer<Object> errorHandlingValueDeserializer
+                = new ErrorHandlingDeserializer<>(valueDeserializer);
+
+        return new DefaultKafkaConsumerFactory<>(
+                configuration,
+                errorHandlingKeyDeserializer,
+                errorHandlingValueDeserializer
+        );
+    }
+
+    <T> ConcurrentKafkaListenerContainerFactory<String, T> getContainer(ConsumerProperties consumerProperties) {
+
+        ConcurrentKafkaListenerContainerFactory<String, T> containerFactory =
+                new ConcurrentKafkaListenerContainerFactory<>();
+
+        containerFactory.setConsumerFactory(getConsumerFactory(consumerProperties));
+        containerFactory.setErrorHandler((thrownException, consumerRecord) -> {
+            final var data = (consumerRecord == null) ? "empty" : consumerRecord.toString();
+            log.error("ERROR, consumerRecord [{}]", data, thrownException);
+        });
+        containerFactory.getContainerProperties().setAckMode(ContainerProperties.AckMode.MANUAL_IMMEDIATE);
+        return containerFactory;
+
+    }
+
+}
--- src/main/java/com/nlmk/kb/server/config/broker/PdmBrokerConfig.java
+++ src/main/java/com/nlmk/kb/server/config/broker/PdmBrokerConfig.java
@@ -1,81 +1,17 @@
 package com.nlmk.kb.server.config.broker;
 
-import io.confluent.kafka.serializers.KafkaAvroDeserializer;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
-import org.springframework.kafka.core.ConsumerFactory;
-import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
-import org.springframework.kafka.listener.ContainerProperties;
-import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer;
 
-import java.util.HashMap;
-import java.util.Map;
-
-@Slf4j
 @Configuration
-@RequiredArgsConstructor
-public class PdmBrokerConfig {
-
-    private final PdmConsumerProperties consumerProperties;
-
-    private ConsumerFactory<Object, Object> pdmConsumerFactory() {
-        Map<String, Object> configuration = new HashMap<>();
-
-        configuration.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, consumerProperties.getKafkaServer());
-        configuration.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, KafkaAvroDeserializer.class);
-        configuration.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, KafkaAvroDeserializer.class);
-        configuration.put(ConsumerConfig.GROUP_ID_CONFIG, consumerProperties.getKafkaGroupId());
-        configuration.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
-        configuration.put("schema.registry.url", consumerProperties.getSchemaRegistryUrl());
-        configuration.put("specific.avro.reader", "true");
-
-        log.info("SSL {}", consumerProperties.isSslEnabled() ? "enabled" : "disabled");
-        if (consumerProperties.isSslEnabled()) {
-            configuration.put("security.protocol", "SSL");
-            configuration.put("ssl.truststore.location", consumerProperties.getTruststorePath());
-            configuration.put("ssl.truststore.password", consumerProperties.getTruststorePassword());
-            configuration.put("ssl.keystore.password", consumerProperties.getKeystorePassword());
-            configuration.put("ssl.keystore.location", consumerProperties.getKeystorePath());
-            configuration.put("ssl.endpoint.identification.algorithm", "");
-        }
-
-        KafkaAvroDeserializer keyDeserializer = new KafkaAvroDeserializer();
-        keyDeserializer.configure(configuration, true);
-
-        KafkaAvroDeserializer valueDeserializer = new KafkaAvroDeserializer();
-        valueDeserializer.configure(configuration, false);
-
-        ErrorHandlingDeserializer<Object> errorHandlingKeyDeserializer
-                = new ErrorHandlingDeserializer<>(keyDeserializer);
-
-        ErrorHandlingDeserializer<Object> errorHandlingValueDeserializer
-                = new ErrorHandlingDeserializer<>(valueDeserializer);
-
-        return new DefaultKafkaConsumerFactory<>(
-                configuration,
-                errorHandlingKeyDeserializer,
-                errorHandlingValueDeserializer
-        );
-    }
+public class PdmBrokerConfig extends BrokerConfigBase {
 
     @Bean
-    public ConcurrentKafkaListenerContainerFactory<String, Object> pdmKafkaListenerContainerFactory() {
+    public ConcurrentKafkaListenerContainerFactory<String, Object> pdmKafkaListenerContainerFactory(PdmConsumerProperties consumerProperties) {
 
-        ConcurrentKafkaListenerContainerFactory<String, Object> factory =
-                new ConcurrentKafkaListenerContainerFactory<>();
+        return getContainer(consumerProperties);
 
-        factory.setConsumerFactory(pdmConsumerFactory());
-        factory.setErrorHandler((thrownException, consumerRecord) -> {
-            final var data = (consumerRecord == null) ? "empty" : consumerRecord.toString();
-            log.error("ERROR, consumerRecord [{}]", data, thrownException);
-        });
-        factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.MANUAL_IMMEDIATE);
-
-        return factory;
     }
 
 }
