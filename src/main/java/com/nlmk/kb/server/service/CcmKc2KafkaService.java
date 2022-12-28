06f843b1b4cfde425b28b48960c5f73b16d3b414
Dmitrii V. Lystsov <demetriuzz@yandex.ru>
2022-12-28 14:55:01 +0500

    #1414 update product-api, avro object for KC1,2

--- src/main/java/com/nlmk/kb/server/service/CcmKc2KafkaService.java
+++ src/main/java/com/nlmk/kb/server/service/CcmKc2KafkaService.java
@@ -8,8 +8,8 @@ import com.nlmk.kb.server.service.result.sending.AttestationResultSender;
 import io.micrometer.core.annotation.Timed;
 import lombok.extern.slf4j.Slf4j;
 import nlmk.l3.apcs.VerificationResultsKc2;
-import nlmk.l3.sus.kc2.AttestRequest;
-import nlmk.l3.sus.kc2.EnumOp;
+import nlmk.EnumOp;
+import nlmk.nlmk.l3.sus.kc2.DbAttestRequestVer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.support.Acknowledgment;
@@ -31,14 +31,14 @@ public class CcmKc2KafkaService {
     private final long sleepTime;
     private final CcmCommonService ccmCommonService;
 
-    private final CcmMessageAdapter<AttestRequest> ccmMessageAdapter;
+    private final CcmMessageAdapter<DbAttestRequestVer> ccmMessageAdapter;
 
     private final AttestationResultSender attestationResultSender;
     private final CcmMessageService ccmMessageService;
 
     public CcmKc2KafkaService(@Value("${kafka.ack.nack.sleep-time}") long sleepTime,
                               CcmCommonService ccmCommonService,
-                              CcmMessageAdapter<AttestRequest> ccmMessageAdapter,
+                              CcmMessageAdapter<DbAttestRequestVer> ccmMessageAdapter,
                               AttestationResultSender attestationResultSender,
                               CcmMessageService ccmMessageService) {
         this.sleepTime = sleepTime;
@@ -57,7 +57,7 @@ public class CcmKc2KafkaService {
                                   @Header(KafkaHeaders.RECEIVED_PARTITION_ID) int partition,
                                   @Header(KafkaHeaders.OFFSET) int offset,
                                   @Header(KafkaHeaders.RECEIVED_TIMESTAMP) String timestamp,
-                                  @Payload AttestRequest request,
+                                  @Payload DbAttestRequestVer request,
                                   Acknowledgment ack) {
 
         log.info("receiveMessageReq (CCM KC2): topic [{}], partition [{}], offset [{}], key [{}], timestamp [{}], request.ts [{}], request.op [{}], request.pk.id [{}]", topic, partition, offset, key, timestamp, request.getTs(), request.getOp(), request.getPk().getId());
--- src/main/java/com/nlmk/kb/server/service/ccm/kc2/CcmKc2MessageAdapterImpl.java
+++ src/main/java/com/nlmk/kb/server/service/ccm/kc2/CcmKc2MessageAdapterImpl.java
@@ -5,7 +5,7 @@ import com.nlmk.kb.server.service.ccm.CcmMessageAdapter;
 import com.nlmk.kb.server.service.ccm.KafkaRequestAdapter;
 import com.nlmk.kb.server.util.SenderUtils;
 import lombok.RequiredArgsConstructor;
-import nlmk.l3.sus.kc2.AttestRequest;
+import nlmk.nlmk.l3.sus.kc2.DbAttestRequestVer;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
@@ -13,12 +13,12 @@ import java.util.Objects;
 
 @Component
 @RequiredArgsConstructor
-public class CcmKc2MessageAdapterImpl implements CcmMessageAdapter<AttestRequest> {
+public class CcmKc2MessageAdapterImpl implements CcmMessageAdapter<DbAttestRequestVer> {
 
-    private final KafkaRequestAdapter<AttestRequest> adapter;
+    private final KafkaRequestAdapter<DbAttestRequestVer> adapter;
 
     @Override
-    public CcmMessage adapt(AttestRequest requestMessage,
+    public CcmMessage adapt(DbAttestRequestVer requestMessage,
                             String topic,
                             String key,
                             int partition,
