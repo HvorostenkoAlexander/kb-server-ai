348aba26ef87cf9a50f98f3d31f0f631836b6792
Леонов Александр Владимирович (leonov_av) <leonov_av@nlmk.com>
2022-12-29 10:21:30 +0300

    Update src/main/resources/db/seeds-psql/V32__add_topics_to_dictionary_config.sql, pom.xml

--- src/main/resources/db/seeds-psql/V32__add_topics_to_dictionary_config.sql
+++ src/main/resources/db/seeds-psql/V32__add_topics_to_dictionary_config.sql
@@ -2,7 +2,7 @@
 -- https://confluence.nlmk.com/pages/viewpage.action?pageId=120629816
 WITH dc AS (
     INSERT INTO dictionary_config (topic, nsi_path, is_enabled)
-        VALUES ('000-0.l3-pdm.cdc.sp-chemical-properties-notes.1',' /nsi/dict/nsd_chemical_properties_notes', true)
+        VALUES ('000-0.l3-pdm.cdc.sp-chemical-properties-notes.1','/nsi/dict/nsd_chemical_properties_notes', true)
         RETURNING id)
 INSERT
 INTO dictionary_config_codes (dictionary_config_id, codes)
