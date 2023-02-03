c2d8ea2f84fa4d0249b48dbf3cb39421b49a3f5f
Aleksandr Vakhrameyev <avakhrameyev@softmedialab.com>
2023-02-03 09:02:14 +0500

    BasePdmCreator refactoring

--- src/main/java/com/nlmk/kb/server/service/pdm/senders/MicrostructureMessageSender.java
+++ src/main/java/com/nlmk/kb/server/service/pdm/senders/MicrostructureMessageSender.java
@@ -1,22 +1,20 @@
 package com.nlmk.kb.server.service.pdm.senders;
 
+import com.nlmk.kb.server.entity.pdm.PdmDictionary;
 import com.nlmk.kb.server.entity.pdm.PdmMessage;
-import com.nlmk.kb.server.entity.Operation;
 import com.nlmk.kb.server.service.pdm.DictionaryConfigService;
 import com.nlmk.kb.server.service.sender.NsiSender;
 import com.nlmk.kb.server.service.pdm.PdmDictionaryCreator;
 import com.nlmk.kb.server.service.pdm.PdmDtoConverter;
-import com.nlmk.kb.server.service.pdm.PdmMessageCreator;
 import lombok.extern.slf4j.Slf4j;
 import nlmk.l3.pdm.SpMicrostructure;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.util.Assert;
 
 @Slf4j
 @Service
-public class MicrostructureMessageSender extends BasePdmCreator implements PdmMessageSender, PdmMessageCreator {
+public class MicrostructureMessageSender extends BasePdmCreator {
 
     public MicrostructureMessageSender(@Value("${kafka.pdm.topic.microstructure}") String type,
                                        NsiSender commonSender,
@@ -27,35 +25,16 @@ public class MicrostructureMessageSender extends BasePdmCreator implements PdmMe
     }
 
     @Override
-    public Long send(PdmMessage message) {
-        Assert.notNull(message, () -> {
-            throw new IllegalArgumentException("message for sending is NULL");
-        });
-
-        return super.getNsiSender().sendBodyReturnLong(
-                super.getPdmDtoConverter().toMicrostructureDto(message.getDictionary()),
-                super.getDictionaryConfigService().getDictionaryUrlByTopic(message.getTopic()),
-                message.getOp()
-        );
+    Object getBody(PdmMessage message) {
+        return super.getPdmDtoConverter().toMicrostructureDto(message.getDictionary());
     }
 
     @Override
-    public PdmMessage createPdmMessage(ConsumerRecord<Object, Object> consumerRecord) {
+    PdmDictionary getDictionary(ConsumerRecord<Object, Object> consumerRecord) {
         final var pdmObject = (SpMicrostructure) consumerRecord.value();
-
-        final var dictionary = super.getPdmDictionaryCreator().createPdmDictionary(
+        return super.getPdmDictionaryCreator().createPdmDictionary(
                 pdmObject.getTs(), pdmObject.getOp(), pdmObject.getPk(), pdmObject.getData()
         );
-
-        return PdmMessage.builder()
-                .topic(consumerRecord.topic())
-                .key((String) consumerRecord.key())
-                .offset(consumerRecord.offset())
-                .partition(consumerRecord.partition())
-                .dictionary(dictionary)
-                .op(Operation.fromValue(dictionary.getOp()))
-                .ts(dictionary.getTs())
-                .build();
     }
 
 }
--- src/main/java/com/nlmk/kb/server/service/pdm/senders/TkNumMessageSender.java
+++ src/main/java/com/nlmk/kb/server/service/pdm/senders/TkNumMessageSender.java
@@ -1,20 +1,18 @@
 package com.nlmk.kb.server.service.pdm.senders;
 
+import com.nlmk.kb.server.entity.pdm.PdmDictionary;
 import com.nlmk.kb.server.entity.pdm.PdmMessage;
-import com.nlmk.kb.server.entity.Operation;
 import com.nlmk.kb.server.service.pdm.DictionaryConfigService;
 import com.nlmk.kb.server.service.sender.NsiSender;
 import com.nlmk.kb.server.service.pdm.PdmDictionaryCreator;
 import com.nlmk.kb.server.service.pdm.PdmDtoConverter;
-import com.nlmk.kb.server.service.pdm.PdmMessageCreator;
 import nlmk.l3.pdm.SpTkNum;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.util.Assert;
 
 @Service
-public class TkNumMessageSender extends BasePdmCreator implements PdmMessageSender, PdmMessageCreator {
+public class TkNumMessageSender extends BasePdmCreator {
 
     public TkNumMessageSender(@Value("${kafka.pdm.topic.tk-num}") String type,
                               NsiSender commonSender,
@@ -25,35 +23,16 @@ public class TkNumMessageSender extends BasePdmCreator implements PdmMessageSend
     }
 
     @Override
-    public Long send(PdmMessage message) {
-        Assert.notNull(message, () -> {
-            throw new IllegalArgumentException("message for sending is NULL");
-        });
-
-        return super.getNsiSender().sendBodyReturnLong(
-                super.getPdmDtoConverter().toTkNumDto(message.getDictionary()),
-                super.getDictionaryConfigService().getDictionaryUrlByTopic(message.getTopic()),
-                message.getOp()
-        );
+    Object getBody(PdmMessage message) {
+        return super.getPdmDtoConverter().toTkNumDto(message.getDictionary());
     }
 
     @Override
-    public PdmMessage createPdmMessage(ConsumerRecord<Object, Object> consumerRecord) {
-        SpTkNum pdmObject = (SpTkNum) consumerRecord.value();
-
-        final var dictionary = super.getPdmDictionaryCreator().createPdmDictionary(
+    PdmDictionary getDictionary(ConsumerRecord<Object, Object> consumerRecord) {
+        final var pdmObject = (SpTkNum) consumerRecord.value();
+        return super.getPdmDictionaryCreator().createPdmDictionary(
                 pdmObject.getTs(), pdmObject.getOp(), pdmObject.getPk(), pdmObject.getData()
         );
-
-        return PdmMessage.builder()
-                .topic(consumerRecord.topic())
-                .key((String) consumerRecord.key())
-                .offset(consumerRecord.offset())
-                .partition(consumerRecord.partition())
-                .dictionary(dictionary)
-                .op(Operation.fromValue(dictionary.getOp()))
-                .ts(dictionary.getTs())
-                .build();
     }
 
 }
