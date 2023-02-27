fbcbb92f39c114bc875747d0ad399c23e34f39ac
Dmitrii V. Lystsov <dlystsov@softmedialab.com>
2023-02-27 12:39:18 +0500

    #1581 AttestationMessageService, add test

--- src/test/java/com/nlmk/kb/server/service/AttestationMessageServiceTest.java
+++ src/test/java/com/nlmk/kb/server/service/AttestationMessageServiceTest.java
@@ -1,8 +1,14 @@
 package com.nlmk.kb.server.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.nlmk.attestation.product.api.ProductDto;
+import com.nlmk.attestation.product.api.RequestDto;
+import com.nlmk.attestation.product.api.pam.ProductAttestationResultDto;
+import com.nlmk.kb.server.api.ccm.pts.CcmPtsRequest;
 import com.nlmk.kb.server.repository.AttestationMessageRepository;
 import com.nlmk.kb.server.repository.CcmMessageSourceRepository;
 import com.nlmk.kb.server.service.sender.PamSender;
+import okhttp3.mockwebserver.MockResponse;
 import okhttp3.mockwebserver.MockWebServer;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Assertions;
@@ -10,10 +16,17 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 
 import java.io.IOException;
+import java.math.BigDecimal;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
 
 @SpringBootTest
 public class AttestationMessageServiceTest {
@@ -28,6 +41,7 @@ public class AttestationMessageServiceTest {
     private AttestationMessageService attestationMessageService;
 
     static MockWebServer mockWebServer;
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
     @DynamicPropertySource
     static void properties(DynamicPropertyRegistry dpr) {
@@ -45,10 +59,54 @@ public class AttestationMessageServiceTest {
         mockWebServer.shutdown();
     }
 
+    private CcmPtsRequest prepareCcmPtsRequest() {
+        final SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSXXX");
+        return CcmPtsRequest.builder()
+                .ts(sdf.format(new Date(1000000000_000L))) // для теста!
+                .pk(CcmPtsRequest.Pk.builder().systemCode("11").id("22").build())
+                .data(CcmPtsRequest.Record.builder()
+                        .marking(CcmPtsRequest.Marking.builder()
+                                .nplv(2106684).hnum(25217).tnum(22).roll(1).build())
+                        .geometry(CcmPtsRequest.Geometry.builder()
+                                .thickness(BigDecimal.valueOf(30.0))
+                                .width(BigDecimal.valueOf(300.0))
+                                .length(BigDecimal.valueOf(3000.0)).build())
+                        .properties(List.of())
+                        .specifications(List.of())
+                        .chemical(List.of())
+                        .build())
+                .build();
+    }
+
     @Test
-    void ccmPtsRequestProcessing() {
-        Assertions.assertThrows(NullPointerException.class, () -> attestationMessageService.ccmPtsRequestProcessing(null));
-        // todo
+    void ccmPtsRequestProcessing() throws Exception {
+        // первоначальный запрос
+        final var request = prepareCcmPtsRequest();
+
+        mockWebServer.enqueue(new MockResponse()
+                .setHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .setResponseCode(HttpStatus.OK.value())
+                .setBody(objectMapper.writeValueAsString(
+                        ProductAttestationResultDto.builder()
+                                .newProduct(true)
+                                .result(ProductDto.builder()
+                                        .id(100L)
+                                        .requests(List.of(RequestDto.builder().id(200L).build()))
+                                        .build())
+                                .build()
+                ))
+        );
+
+        Assertions.assertDoesNotThrow(() -> attestationMessageService.ccmPtsRequestProcessing(request));
+        mockWebServer.takeRequest();
+
+        // сообщение с запросом в формате PAM сохранено
+        final var attMessage = attestationMessageRepository.findFirstByPrimeIdOrderByReceiptTsDesc("22");
+        Assertions.assertTrue(attMessage.isPresent());
+        // сообщение с первоначальным запросом сохранено
+        final var sourceMessage = ccmMessageSourceRepository.findByRequestId(200L);
+        Assertions.assertTrue(sourceMessage.isPresent());
+        Assertions.assertEquals(objectMapper.writeValueAsString(request), sourceMessage.get().getMessageSource());
     }
 
 }
