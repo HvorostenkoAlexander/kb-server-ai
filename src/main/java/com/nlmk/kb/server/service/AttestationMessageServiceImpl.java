dd1ed1760ea1fd5c4a856e67678ab4684e261056
Alexander Leonov <aleonov224@gmail.com>
2023-03-16 11:09:21 +0300

    #1627-rest-api

--- src/main/java/com/nlmk/kb/server/service/AttestationMessageServiceImpl.java
+++ src/main/java/com/nlmk/kb/server/service/AttestationMessageServiceImpl.java
@@ -49,6 +49,30 @@ public class AttestationMessageServiceImpl implements AttestationMessageService 
 
     private final ObjectMapper objectMapper = new ObjectMapper();
 
+    @FunctionalInterface
+    public interface Saver {
+        void save(Long requestId, String primeId) throws JsonProcessingException;
+    }
+    public class SourceMessageSaver<T> implements Saver {
+        private final CcmMessageSourceRepository sourceRepository;
+        private final T sourceMessage;
+        public SourceMessageSaver(CcmMessageSourceRepository sourceRepository, T t) {
+            this.sourceRepository = sourceRepository;
+            this.sourceMessage = t;
+        }
+
+        @Override
+        public void save(Long requestId, String primeId) throws JsonProcessingException {
+            this.sourceRepository.save(
+                    CcmMessageSource.builder()
+                            .requestId(requestId)
+                            .primeId(primeId)
+                            .messageSource(objectMapper.writeValueAsString(this.sourceMessage))
+                            .build()
+            );
+        }
+    }
+
     @Override
     public Optional<AttestationMessage> findLastAttestationMessage(String primeId) {
         return attestationMessageRepository.findFirstByPrimeIdOrderByReceiptTsDesc(primeId);
@@ -63,22 +87,12 @@ public class AttestationMessageServiceImpl implements AttestationMessageService 
     @Override
     @Transactional
     public CcmPtsResponse ccmPtsRequestProcessing(CcmPtsRequest request) {
-        /*
-         * 1. принять запрос на аттестацию
-         *
-         * #processAttestationRequest
-         * 2. преобразовать запрос к общему виду для PAM (com.nlmk.attestation.product.api.pam.AttestationRequest)
-         * 3. отправить запрос на аттестацию в PAM
-         * 4. получить ответ от PAM
-         * 5. сохранить первоисточник запроса в базу (для просмотра запроса в UI)
-         * 6. сохранить запрос в базу (для запуска повторной аттестации по сообщениям САДиМ)
-         * processAttestationRequest#
-         *
-         * 7. преобразовать и отправить ответ
-         */
         log.info("ccmPtsRequestProcessing, request [{}]", request);
         final var attRequest = ccmPtsRestRequestAdapter.adapt(request);
-        return ccmPtsRestResponseAdapter.adapt(processAttestationRequest(attRequest, AttestationMessageSender.CCM_PTS));
+        return ccmPtsRestResponseAdapter.adapt(processAttestationRequest(attRequest,
+                AttestationMessageSender.CCM_PTS,
+                new SourceMessageSaver<>(ccmMessageSourceRepository, request))
+        );
     }
 
     @Override
@@ -100,54 +114,37 @@ public class AttestationMessageServiceImpl implements AttestationMessageService 
     @Override
     @Transactional
     public CcmKc1Response ccmKc1RequestProcessing(CcmKc1Request request) {
-        /*
-         * 1. принять запрос на аттестацию
-         *
-         * #processAttestationRequest
-         * 2. преобразовать запрос к общему виду для PAM (com.nlmk.attestation.product.api.pam.AttestationRequest)
-         * 3. отправить запрос на аттестацию в PAM
-         * 4. получить ответ от PAM
-         * 5. сохранить первоисточник запроса в базу (для просмотра запроса в UI)
-         * 6. сохранить запрос в базу (для запуска повторной аттестации по сообщениям САДиМ)
-         * processAttestationRequest#
-         *
-         * 7. преобразовать и отправить ответ
-         */
         log.info("ccKc1RequestProcessing, request [{}]", request);
         final var attRequest = ccmKc1RestRequestAdapter.adapt(request);
-        return ccmKc1RestResponseAdapter.adapt(processAttestationRequest(attRequest, AttestationMessageSender.CCM_KC2));
+        return ccmKc1RestResponseAdapter.adapt(processAttestationRequest(attRequest,
+                AttestationMessageSender.CCM_KC1,
+                new SourceMessageSaver<>(ccmMessageSourceRepository, request))
+        );
     }
 
     @Override
     @Transactional
     public CcmKc2Response ccmKc2RequestProcessing(CcmKc2Request request) {
-        /*
-         * 1. принять запрос на аттестацию
-         *
-         * #processAttestationRequest
-         * 2. преобразовать запрос к общему виду для PAM (com.nlmk.attestation.product.api.pam.AttestationRequest)
-         * 3. отправить запрос на аттестацию в PAM
-         * 4. получить ответ от PAM
-         * 5. сохранить первоисточник запроса в базу (для просмотра запроса в UI)
-         * 6. сохранить запрос в базу (для запуска повторной аттестации по сообщениям САДиМ)
-         * processAttestationRequest#
-         *
-         * 7. преобразовать и отправить ответ
-         */
         log.info("ccKc2RequestProcessing, request [{}]", request);
         final var attRequest = ccmKc2RestRequestAdapter.adapt(request);
-        return ccmKc2RestResponseAdapter.adapt(processAttestationRequest(attRequest, AttestationMessageSender.CCM_KC2));
+        return ccmKc2RestResponseAdapter.adapt(processAttestationRequest(attRequest,
+                AttestationMessageSender.CCM_KC2,
+                new SourceMessageSaver<>(ccmMessageSourceRepository, request))
+        );
     }
 
 
     private ProductAttestationResultDto processAttestationRequest(AttestationRequest attestationRequest,
-                                                                  AttestationMessageSender sender) {
+                                                                  AttestationMessageSender sender,
+                                                                  Saver sourceSaver) {
         /*
+         * 1. принять запрос на аттестацию
          * 2. преобразовать запрос к общему виду для PAM (com.nlmk.attestation.product.api.pam.AttestationRequest)
          * 3. отправить запрос на аттестацию в PAM
          * 4. получить ответ от PAM
          * 5. сохранить первоисточник запроса в базу (для просмотра запроса в UI)
          * 6. сохранить запрос в базу (для запуска повторной аттестации по сообщениям САДиМ)
+         * 7. преобразовать и отправить ответ
          */
         final var primeId = SenderUtils.getPrimeId(attestationRequest);
 
@@ -166,13 +163,7 @@ public class AttestationMessageServiceImpl implements AttestationMessageService 
                     && !CollectionUtils.isEmpty(attResult.getResult().getRequests())) {
                 final var requestId = attResult.getResult().getRequests().get(0).getId();
                 if (Objects.nonNull(requestId)) {
-                    ccmMessageSourceRepository.save(
-                            CcmMessageSource.builder()
-                                    .requestId(requestId)
-                                    .primeId(primeId)
-                                    .messageSource(objectMapper.writeValueAsString(attestationRequest))
-                                    .build()
-                    );
+                    sourceSaver.save(requestId, primeId);
                 }
             }
 
