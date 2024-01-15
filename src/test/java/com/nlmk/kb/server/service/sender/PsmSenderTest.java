c8b92a19a4a38c855e3fe60870241db100a14a82
Демьянчик Андрей Павлович (demyanchik_ap) <demyanchik_ap@nlmk.com>
2024-01-15 16:36:51 +0300

    [APCS-232] [feat] Implement psm sender method for zmmorder

--- src/test/java/com/nlmk/kb/server/service/sender/PsmSenderTest.java
+++ src/test/java/com/nlmk/kb/server/service/sender/PsmSenderTest.java
@@ -1,6 +1,7 @@
 package com.nlmk.kb.server.service.sender;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.nlmk.attestation.zmmorder.ZMMORDERS05DOP;
 import com.nlmk.attestation.product.api.SadimMessageDto;
 import com.nlmk.attestation.zorder.ZORDERS051;
 import com.nlmk.kb.server.exception.RemoteServiceSenderException;
@@ -49,75 +50,142 @@ class PsmSenderTest {
         mockWebServer.shutdown();
     }
 
+    private static final SadimMessageDto SADIM_MESSAGE_DTO = SadimMessageDto.builder().partition(0).offset(1L).key("key")
+            .param(SadimMessageDto.ParamDto.builder().primeId("pi100").build())
+            .build();
+
     @Test
-    void postZorderTest() throws InterruptedException, IOException {
+    void postZorderToPsmShouldThrowRemoteServiceSenderExceptionIfResponseCodeIsBadRequest() throws IOException, InterruptedException {
+        // given
         mockWebServer.enqueue(new MockResponse()
                 .setHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
                 .setResponseCode(HttpStatus.BAD_REQUEST.value())
         );
-
         final var zorders051 = objectMapper.readValue(new ClassPathResource("json/zordersExample.json").getFile(), ZORDERS051.class);
-
+        // when
+        // then
         assertThrows(RemoteServiceSenderException.class, () -> psmSender.postZorder(zorders051));
         mockWebServer.takeRequest();
+    }
 
+    @Test
+    void canPostZorderToPsm() throws InterruptedException, IOException {
+        // given
         mockWebServer.enqueue(new MockResponse()
                 .setHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
                 .setResponseCode(HttpStatus.OK.value())
                 .setBody("100")
         );
+        final var zorders051 = objectMapper.readValue(new ClassPathResource("json/zordersExample.json").getFile(), ZORDERS051.class);
 
+        // when
         var result = psmSender.postZorder(zorders051);
 
-        RecordedRequest request2 = mockWebServer.takeRequest();
-        assertEquals("POST", request2.getMethod());
-        assertEquals("/sap/order", request2.getPath());
+        // then
+        RecordedRequest request = mockWebServer.takeRequest();
+        assertEquals("POST", request.getMethod());
+        assertEquals("/sap/zorder", request.getPath());
         assertEquals(100, result);
 
-        String body = request2.getBody().readUtf8();
+        String body = request.getBody().readUtf8();
         final ZORDERS051 zorderFromBody = objectMapper.readValue(body, ZORDERS051.class);
         assertEquals("0040452892", zorderFromBody.getIDOC().getE1EDK01().getBELNR());
     }
 
     @Test
-    void postSadimMessage() throws Exception {
+    void postZmmorderToPsmShouldThrowRemoteServiceSenderExceptionIfResponseCodeIsBadRequest() throws IOException, InterruptedException {
+        // given
         mockWebServer.enqueue(new MockResponse()
                 .setHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
                 .setResponseCode(HttpStatus.BAD_REQUEST.value())
         );
-        final var dto = SadimMessageDto.builder().partition(0).offset(1L).key("key")
-                .param(SadimMessageDto.ParamDto.builder().primeId("pi100").build())
-                .build();
-
-        final var response1 = assertThrows(RemoteServiceSenderException.class, () -> psmSender.postSadimMessage(dto));
-        assertEquals("PsmSender, postSadimMessage, primeId [pi100], send error, message [PSM return code [400]]", response1.getMessage());
+        final var zmmorder = objectMapper.readValue(new ClassPathResource("json/zmmordersExample.json").getFile(), ZMMORDERS05DOP.class);
+        // when
+        // then
+        assertThrows(RemoteServiceSenderException.class, () -> psmSender.postZmmorder(zmmorder));
         mockWebServer.takeRequest();
+    }
 
+    @Test
+    void canPostZmmorderToPsm() throws InterruptedException, IOException {
+        // given
+        mockWebServer.enqueue(new MockResponse()
+                .setHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .setResponseCode(HttpStatus.OK.value())
+                .setBody("20")
+        );
+        final var zmmorder = objectMapper.readValue(new ClassPathResource("json/zmmordersExample.json").getFile(), ZMMORDERS05DOP.class);
+
+        // when
+        var result = psmSender.postZmmorder(zmmorder);
+
+        // then
+        RecordedRequest request = mockWebServer.takeRequest();
+        assertEquals("POST", request.getMethod());
+        assertEquals("/sap/zmmorder", request.getPath());
+        assertEquals(20, result);
+
+        String body = request.getBody().readUtf8();
+        final ZMMORDERS05DOP zorderFromBody = objectMapper.readValue(body, ZMMORDERS05DOP.class);
+        assertEquals("4500745477", zorderFromBody.getIDOC().getE1EDK01().getBELNR());
+    }
+
+    @Test
+    void postSadimMessageShouldThrowRemoteServiceSenderExceptionIfResponseCodeIsBadRequest() throws InterruptedException {
+        // given
+        mockWebServer.enqueue(new MockResponse()
+                .setHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .setResponseCode(HttpStatus.BAD_REQUEST.value())
+        );
+        // when
+        final var response = assertThrows(RemoteServiceSenderException.class, () -> psmSender.postSadimMessage(SADIM_MESSAGE_DTO));
+        // then
+        assertEquals("PsmSender, postSadimMessage, primeId [pi100], send error, message [PSM return code [400]]", response.getMessage());
+        mockWebServer.takeRequest();
+    }
+
+    @Test
+    void postSadimMessageShouldThrowRemoteServiceSenderExceptionIfResponseCodeIsUnauthorized() throws InterruptedException {
+        // given
+        mockWebServer.enqueue(new MockResponse()
+                .setHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .setResponseCode(HttpStatus.UNAUTHORIZED.value())
+        );
+        // when
+        final var response = assertThrows(RemoteServiceSenderException.class, () -> psmSender.postSadimMessage(SADIM_MESSAGE_DTO));
+        // then
+        assertEquals("PsmSender, postSadimMessage, primeId [pi100], send error, message [PSM return code [401]]", response.getMessage());
+        mockWebServer.takeRequest();
+    }
+
+    @Test
+    void postSadimMessageShouldThrowRemoteServiceSenderExceptionIfResponseCodeIsForbidden() throws InterruptedException {
+        // given
+        mockWebServer.enqueue(new MockResponse()
+                .setHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .setResponseCode(HttpStatus.FORBIDDEN.value())
+        );
+        // when
+        final var response = assertThrows(RemoteServiceSenderException.class, () -> psmSender.postSadimMessage(SADIM_MESSAGE_DTO));
+        // then
+        assertEquals("PsmSender, postSadimMessage, primeId [pi100], send error, message [PSM return code [403]]", response.getMessage());
+        mockWebServer.takeRequest();
+    }
+
+    @Test
+    void canPostSadimMessage() throws Exception {
+        // given
         mockWebServer.enqueue(new MockResponse()
                 .setHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
                 .setResponseCode(HttpStatus.CREATED.value())
         );
 
-        assertDoesNotThrow(() -> psmSender.postSadimMessage(dto));
-        RecordedRequest request2 = mockWebServer.takeRequest();
-        assertEquals("POST", request2.getMethod());
-        assertEquals("/sadim", request2.getPath());
+        // when
+        assertDoesNotThrow(() -> psmSender.postSadimMessage(SADIM_MESSAGE_DTO));
 
-        mockWebServer.enqueue(new MockResponse()
-                .setHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
-                .setResponseCode(HttpStatus.UNAUTHORIZED.value())
-        );
-        final var response3 = assertThrows(RemoteServiceSenderException.class, () -> psmSender.postSadimMessage(dto));
-        assertEquals("PsmSender, postSadimMessage, primeId [pi100], send error, message [PSM return code [401]]", response3.getMessage());
-        mockWebServer.takeRequest();
-
-        mockWebServer.enqueue(new MockResponse()
-                .setHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
-                .setResponseCode(HttpStatus.FORBIDDEN.value())
-        );
-        final var response4 = assertThrows(RemoteServiceSenderException.class, () -> psmSender.postSadimMessage(dto));
-        assertEquals("PsmSender, postSadimMessage, primeId [pi100], send error, message [PSM return code [403]]", response4.getMessage());
-        mockWebServer.takeRequest();
+        // then
+        RecordedRequest request = mockWebServer.takeRequest();
+        assertEquals("POST", request.getMethod());
+        assertEquals("/sadim", request.getPath());
     }
-
 }
