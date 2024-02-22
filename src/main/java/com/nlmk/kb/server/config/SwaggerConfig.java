8ea5e5ca3e59fa194a45f6099c302309c5bd62fc
Демьянчик Андрей Павлович (demyanchik_ap) <demyanchik_ap@nlmk.com>
2024-02-22 12:09:00 +0300

    [APCS-269] [feat] Change clientCredentials auth method to authorizationCode oauth2 flow

--- src/main/java/com/nlmk/kb/server/config/SwaggerConfig.java
+++ src/main/java/com/nlmk/kb/server/config/SwaggerConfig.java
@@ -2,6 +2,7 @@ package com.nlmk.kb.server.config;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.*;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -16,9 +17,9 @@ public class SwaggerConfig {
     private final String realm;
 
     public SwaggerConfig(@Value("${keycloak.auth-server-url}")
-                                 String authServerUrl,
+                         String authServerUrl,
                          @Value("${keycloak.realm}")
-                                 String realm) {
+                         String realm) {
         this.authServerUrl = authServerUrl;
         this.realm = realm;
     }
@@ -30,14 +31,17 @@ public class SwaggerConfig {
         final var token = "/protocol/openid-connect/token";
 
         return new OpenAPI()
+                .info(new Info()
+                        .title("Kafka Broker (KB) Service")
+                        .version("1.0.0")
+                        .description("Модуль интеграции с внешними системами"))
                 .components(new Components()
                         .addSecuritySchemes("bearer-key", new SecurityScheme()
                                 .type(SecurityScheme.Type.OAUTH2)
                                 .description("Oauth2 flow")
                                 .flows(new OAuthFlows()
-                                        .clientCredentials(new OAuthFlow()
+                                        .authorizationCode(new OAuthFlow()
                                                 .authorizationUrl(authServerUrl + realms + realm + auth)
-                                                .refreshUrl(authServerUrl + realms + realm + token)
                                                 .tokenUrl(authServerUrl + realms + realm + token)
                                                 .scopes(new Scopes())
                                         ))
