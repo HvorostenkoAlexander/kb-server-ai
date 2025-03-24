e9d4a33438d9e41a391bbd9deb6ef2fb51d47a5b
Vladislav Kosobokov <v.kosobokov@iteco-inno.ru>
2025-03-24 18:12:05 +0300

    [APCS-833] [fix] CDC Endpoint fix

--- src/main/java/com/nlmk/kb/server/service/CaptureDataChangeServiceImpl.java
+++ src/main/java/com/nlmk/kb/server/service/CaptureDataChangeServiceImpl.java
@@ -1,5 +1,6 @@
 package com.nlmk.kb.server.service;
 
+import com.nlmk.attestation.product.api.kb.CdcUpdate;
 import com.nlmk.kb.server.repository.MdmMessageRepository;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -12,11 +13,10 @@ public class CaptureDataChangeServiceImpl implements CaptureDataChangeService {
 
     private final MdmMessageRepository mdmMessageRepository;
     @Override
-    public void updateMdmMessage(String message) {
-        String[] msg = message.split(":");
-        var mdmMessage = mdmMessageRepository.findById(Long.parseLong(msg[1]));
+    public void updateMdmMessage(CdcUpdate cdcUpdate) {
+        var mdmMessage = mdmMessageRepository.findById(Long.parseLong(cdcUpdate.getMessageId()));
         if (mdmMessage.isPresent()) {
-            mdmMessage.get().setNote(msg[0]);
+            mdmMessage.get().setNote(cdcUpdate.getStatus());
             mdmMessageRepository.save(mdmMessage.get());
         }
     }
