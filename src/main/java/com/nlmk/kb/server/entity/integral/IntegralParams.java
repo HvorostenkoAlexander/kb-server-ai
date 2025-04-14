cf5e1eab618f96d248cba983dfaba5863d502d88
Спивак Вадим Вячеславович (spivak_vv) <spivak_vv@nlmk.com>
2025-04-14 10:14:09 +0300

    Revert "Merge branch 'revert-3799bee6' into 'development'"
    This reverts merge request !252

---
+++ src/main/java/com/nlmk/kb/server/entity/integral/IntegralParams.java
@@ -0,0 +1,21 @@
+package com.nlmk.kb.server.entity.integral;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class IntegralParams {
+
+    private UUID attrId;
+    private Long attrCode;
+    private BigDecimal attrValue;
+
+}
