a56d4aa4c575b82bd96831292295119f40bbbc47
Vladislav Kosobokov <v.kosobokov@iteco-inno.ru>
2025-04-30 12:24:44 +0300

    Ccm Pgp: disable for test broker and consumer properties beans

--- src/main/java/com/nlmk/kb/server/config/broker/CcmPgpConsumerProperties.java
+++ src/main/java/com/nlmk/kb/server/config/broker/CcmPgpConsumerProperties.java
@@ -2,8 +2,10 @@ package com.nlmk.kb.server.config.broker;
 
 import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
+@Profile("!test")
 @Getter
 @Component
 public class CcmPgpConsumerProperties extends ConsumerProperties {
