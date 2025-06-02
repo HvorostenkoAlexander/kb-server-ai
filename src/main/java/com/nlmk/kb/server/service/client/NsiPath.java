def5e28f1d074c55764f400f0cb831f88002279e
Vladislav Kosobokov <v.kosobokov@iteco-inno.ru>
2025-06-02 11:39:15 +0300

    [APCS-952] [feat] Реализация изменений, требуемых для отправки идентификатора статуса аттестации параметров в MES

--- src/main/java/com/nlmk/kb/server/service/client/NsiPath.java
+++ src/main/java/com/nlmk/kb/server/service/client/NsiPath.java
@@ -8,7 +8,8 @@ import lombok.Getter;
 public enum NsiPath {
 
     GET_ATTRIBUTE_ATTESTATION_GROUP("/nsi/dict/mdm/sp_attribute_attestation_group"),
-    GET_ATTRIBUTES("/nsi/dict/mdm/sp_attributes");
+    GET_ATTRIBUTES("/nsi/dict/mdm/sp_attributes"),
+    GET_APCS_ATTESTATION_RESULT("/nsi/dict/mdm/sp_apcs_attestation_result");
 
     private final String value;
 }
