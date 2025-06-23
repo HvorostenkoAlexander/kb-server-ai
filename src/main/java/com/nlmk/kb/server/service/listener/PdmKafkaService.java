4015c90492714d3a5a626f9e5bb5b94de1580853
Спивак Вадим Вячеславович (spivak_vv) <spivak_vv@nlmk.com>
2025-06-23 13:53:36 +0300

    Revert "[APCS-988][feat] Update CCM-PGP, MES-PGP result topics for test"

--- src/main/java/com/nlmk/kb/server/service/listener/PdmKafkaService.java
+++ src/main/java/com/nlmk/kb/server/service/listener/PdmKafkaService.java
@@ -84,8 +84,7 @@ public class PdmKafkaService {
             throw new DateTimeParseException(MessageFormat.format(KbConstants.LISTENER_EXC_MESSAGE_TEMPLATE, e));
         } catch (Exception e) {
             log.warn("receiveMessageReq, Exception", e);
-            //ack.nack(sleepTime);
-            ack.acknowledge();
+            ack.nack(sleepTime);
             throw new KafkaMessageProcessingException(MessageFormat.format(KbConstants.LISTENER_EXC_MESSAGE_TEMPLATE, e));
         }
     }
